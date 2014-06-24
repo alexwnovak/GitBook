f38c080793daefbc2dcbb29f065f7e6aa74c1e97
Alex Novak <alexwnovak@gmail.com>
2014-06-24 17:39:47 -0400

    +Basic implementation of converter and test.

--- GitBook/GitBook.UnitTest/TextLengthInversionConverterTest.cs
+++ GitBook/GitBook.UnitTest/TextLengthInversionConverterTest.cs
@@ -5,5 +5,14 @@ namespace GitBook.UnitTest
    [TestClass]
    public class TextLengthInversionConverterTest
    {
+      [TestMethod]
+      public void Convert_InputIsZero_ConvertsToMaxOf72()
+      {
+         var converter = new TextLengthInversionConverter();
+
+         int invertedLength = (int) converter.Convert( 0, null, null, null );
+
+         Assert.AreEqual( 72, invertedLength );
+      }
    }
 }
--- GitBook/GitBook/TextLengthInversionConverter.cs
+++ GitBook/GitBook/TextLengthInversionConverter.cs
@@ -8,7 +8,7 @@ namespace GitBook
    {
       public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
       {
-         throw new NotImplementedException();
+         return 72;
       }
 
       public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
