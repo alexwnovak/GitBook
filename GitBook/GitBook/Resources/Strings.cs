bec95ed1546c3f67df6cb7e6d79d793c90520acc
Alex Novak <alexwnovak@gmail.com>
2014-06-24 23:17:18 -0400

    +Strings static class as message provider.

---
+++ GitBook/GitBook/Resources/Strings.cs
@@ -0,0 +1,13 @@
+﻿namespace GitBook.Resources
+{
+   public static class Strings
+   {
+      public static string ConfirmDiscardMessage
+      {
+         get
+         {
+            return "Abort this commit?";
+         }
+      }
+   }
+}
