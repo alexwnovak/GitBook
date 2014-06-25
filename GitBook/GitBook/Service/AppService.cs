14b309f1dc300b7bbdcdcd2273cf97e23b49622e
Alex Novak <alexwnovak@gmail.com>
2014-06-24 22:31:22 -0400

    +AppService default implementation of IAppService.

---
+++ GitBook/GitBook/Service/AppService.cs
@@ -0,0 +1,10 @@
+namespace GitBook.Service
+{
+   public class AppService : IAppService
+   {
+      public void Shutdown()
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
