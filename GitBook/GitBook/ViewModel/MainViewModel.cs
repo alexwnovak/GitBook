4e63aee2e68478cc3c73de4c5435707d07995762
Alex Novak <alexwnovak@gmail.com>
2014-06-24 22:34:01 -0400

    +Command for capturing the key down event.

--- GitBook/GitBook/ViewModel/MainViewModel.cs
+++ GitBook/GitBook/ViewModel/MainViewModel.cs
@@ -1,10 +1,23 @@
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace GitBook.ViewModel
 {
    public class MainViewModel : ViewModelBase
    {
+      public RelayCommand<KeyEventArgs> CommitNotesKeyDownCommand
+      {
+         get;
+         private set;
+      }
+       
       public MainViewModel()
+      {
+         CommitNotesKeyDownCommand = new RelayCommand<KeyEventArgs>( OnCommitNotesKeyDown );
+      }
+
+      public void OnCommitNotesKeyDown( KeyEventArgs e )
       {
       }
    }
