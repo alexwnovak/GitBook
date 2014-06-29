1030b12b9f9f8ce536d6c05602de495484521149
Alex Novak <alexwnovak@gmail.com>
2014-06-29 13:09:00 -0400

    +BeginStoryboard method and implementation for AppService.
    Acts as a simple pass-through / wrapper for activating storyboards.

--- GitBook/GitBook/Service/AppService.cs
+++ GitBook/GitBook/Service/AppService.cs
@@ -1,9 +1,17 @@
 using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace GitBook.Service
 {
    public class AppService : IAppService
    {
+      public void BeginStoryboard( string storyboardName )
+      {
+         var storyboard = (Storyboard) Application.Current.MainWindow.Resources[storyboardName];
+
+         storyboard.Begin();
+      }
+
       public MessageBoxResult DisplayMessageBox( string message, MessageBoxButton buttons )
       {
          return MessageBox.Show( message, null, buttons );
--- GitBook/GitBook/Service/IAppService.cs
+++ GitBook/GitBook/Service/IAppService.cs
@@ -4,6 +4,8 @@ namespace GitBook.Service
 {
    public interface IAppService
    {
+      void BeginStoryboard( string storyboardName );
+
       MessageBoxResult DisplayMessageBox( string message, MessageBoxButton buttons );
 
       void Shutdown();
