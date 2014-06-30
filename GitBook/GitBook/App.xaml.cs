779b7d3cd5f559c8aacc713e580bb2f29f717640
Alex Novak <alexwnovak@gmail.com>
2014-06-29 22:30:44 -0400

    +Wireup for CommitFileReader.

--- GitBook/GitBook/App.xaml.cs
+++ GitBook/GitBook/App.xaml.cs
@@ -15,6 +15,7 @@ namespace GitBook
          SimpleIoc.Default.Register<MainViewModel>();
          SimpleIoc.Default.Register<IAppService, AppService>();
          SimpleIoc.Default.Register<IEnvironmentAdapter, EnvironmentAdapter>();
+         SimpleIoc.Default.Register<ICommitFileReader, CommitFileReader>();
 
          // Load the commit file
 
