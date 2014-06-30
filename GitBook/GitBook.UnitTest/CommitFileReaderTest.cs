7eb39bafcc7c97b9400a269aff43f849923f12b7
Alex Novak <alexwnovak@gmail.com>
2014-06-29 23:01:11 -0400

    CommitFileReader now throws if the file does not exist. +Test

---
+++ GitBook/GitBook.UnitTest/CommitFileReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GitBook.UnitTest
+{
+   [TestClass]
+   public class CommitFileReaderTest
+   {
+      [TestCleanup]
+      public void Cleanup()
+      {
+         SimpleIoc.Default.Reset();
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( GitFileLoadException ) )]
+      public void FromFile_FileDoesNotExist_ThrowsGitFileLoadException()
+      {
+         // Setup
+
+         var fileAdapterMock = new Mock<IFileAdapter>();
+         SimpleIoc.Default.Register( () => fileAdapterMock.Object );
+
+         // Test
+
+         var commitFileReader = new CommitFileReader();
+
+         commitFileReader.FromFile( "SomeFile" );
+      }
+   }
+}
--- GitBook/GitBook/CommitFileReader.cs
+++ GitBook/GitBook/CommitFileReader.cs
@@ -1,9 +1,20 @@
-﻿namespace GitBook
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace GitBook
 {
    public class CommitFileReader : ICommitFileReader
    {
       public CommitDocument FromFile( string path )
       {
+         var fileAdapter = SimpleIoc.Default.GetInstance<IFileAdapter>();
+
+         bool fileExists = fileAdapter.Exists( path );
+
+         if ( !fileExists )
+         {
+            throw new GitFileLoadException();
+         }
+
          return null;
       }
    }
