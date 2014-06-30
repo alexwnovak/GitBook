393b550afdeca6434ebc3267a5f6ca7abbd0eb0a
Alex Novak <alexwnovak@gmail.com>
2014-06-29 21:50:11 -0400

    AppController now exits the app if arguments are null. +test

---
+++ GitBook/GitBook.UnitTest/AppControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GitBook.UnitTest
+{
+   [TestClass]
+   public class AppControllerTest
+   {
+      [TestCleanup]
+      public void Cleanup()
+      {
+         SimpleIoc.Default.Reset();
+      }
+
+      [TestMethod]
+      public void Start_CommandLineArgumentsAreNull_ExitsWithCodeOne()
+      {
+         // Setup
+
+         var environmentAdapterMock = new Mock<IEnvironmentAdapter>();
+         SimpleIoc.Default.Register( () => environmentAdapterMock.Object );
+
+         // Test
+
+         var appController = new AppController();
+
+         appController.Start( null );
+
+         // Assert
+
+         environmentAdapterMock.Verify( ea => ea.Exit( 1 ), Times.Once() );
+      }
+   }
+}
--- GitBook/GitBook/AppController.cs
+++ GitBook/GitBook/AppController.cs
@@ -1,10 +1,17 @@
-﻿namespace GitBook
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace GitBook
 {
    public class AppController
    {
       public void Start( string[] arguments )
       {
-         
+         if ( arguments == null )
+         {
+            var environmentAdapter = SimpleIoc.Default.GetInstance<IEnvironmentAdapter>();
+
+            environmentAdapter.Exit( 1 );
+         }
       }
    }
 }
