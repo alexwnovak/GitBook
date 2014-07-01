89dec0c685e9cc751898b309c0f62bac3604bd42
Alex Novak <alexwnovak@gmail.com>
2014-07-01 12:54:39 -0400

    +HelpControl blank class.

---
+++ GitBook/GitBook/HelpControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GitBook
+{
+   public partial class HelpControl : UserControl
+   {
+      public HelpControl()
+      {
+         InitializeComponent();
+      }
+   }
+}
