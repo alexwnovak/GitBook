c0f2fcaa7d20d2a4eca61a9a2b0175fdc2b17710
Alex Novak <alexwnovak@gmail.com>
2015-11-01 20:11:53 -0500

    Can now get help text for a corresponding commit UI state

--- src/GitWrite/GitWrite.UnitTests/HelpTextProviderTests.cs
+++ src/GitWrite/GitWrite.UnitTests/HelpTextProviderTests.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GitWrite.Resources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GitWrite.UnitTests
 {
    [TestClass]
    public class HelpTextProviderTests
    {
+      [TestMethod]
+      public void GetTextForCommitState_EditingPrimaryMessage_ReturnsCorrespondingHelpText()
+      {
+         string helpText = HelpTextProvider.GetTextForCommitState( CommitControlState.EditingPrimaryMessage );
+
+         Assert.AreEqual( Strings.CommitEditingPrimaryMessageText, helpText );
+      }
+
+      [TestMethod]
+      public void GetTextForCommitState_EditingSecondaryNotes_ReturnsCorrespondingHelpText()
+      {
+         string helpText = HelpTextProvider.GetTextForCommitState( CommitControlState.EditingSecondaryNotes );
+
+         Assert.AreEqual( Strings.CommitEditingSecondaryNotesText, helpText );
+      }
    }
 }
--- src/GitWrite/GitWrite/HelpTextProvider.cs
+++ src/GitWrite/GitWrite/HelpTextProvider.cs
@@ -1,6 +1,21 @@
-﻿namespace GitWrite
+﻿using System;
+using GitWrite.Resources;
+
+namespace GitWrite
 {
    public static class HelpTextProvider
    {
+      public static string GetTextForCommitState( CommitControlState state )
+      {
+         switch ( state )
+         {
+            case CommitControlState.EditingPrimaryMessage:
+               return Strings.CommitEditingPrimaryMessageText;
+            case CommitControlState.EditingSecondaryNotes:
+               return Strings.CommitEditingSecondaryNotesText;
+         }
+
+         throw new ArgumentException( $"Unknown state: {state}", nameof( state ) );
+      }
    }
 }
