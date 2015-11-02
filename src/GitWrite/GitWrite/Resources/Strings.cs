fd3650329efb2c35629f19501675f174dda838d8
Alex Novak <alexwnovak@gmail.com>
2015-11-01 20:09:23 -0500

    +Messaging for commit states

--- src/GitWrite/GitWrite/Resources/Strings.cs
+++ src/GitWrite/GitWrite/Resources/Strings.cs
@@ -3,5 +3,9 @@
    public static class Strings
    {
       public static string ConfirmDiscardMessage => "Abort this commit?";
+
+      public static string CommitEditingPrimaryMessageText => "[Tab] for secondary notes  |  [Enter] to commit";
+
+      public static string CommitEditingSecondaryNotesText => "[Ctrl + Enter] to commit";
    }
 }
