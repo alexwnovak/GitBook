6d4e92cf17f51e9278c308a00574886819178c45
Alex Novak <alexwnovak@gmail.com>
2015-11-02 11:51:06 -0500

    +Test for the short and long message (3 lines) with no line break

--- src/GitWrite/GitWrite.UnitTests/CommitFileReaderTests.cs
+++ src/GitWrite/GitWrite.UnitTests/CommitFileReaderTests.cs
@@ -465,5 +465,50 @@ namespace GitWrite.UnitTests
          Assert.AreEqual( longMessage, commitDocument.LongMessage[0] );
          Assert.AreEqual( longMessage2, commitDocument.LongMessage[1] );
       }
+
+      [TestMethod]
+      public void FromFile_FileHasLongMessageWithThreeLinesAndNoSpaceFromTheShortMessage_SetsShortAndLongMessage()
+      {
+         const string path = "COMMIT_EDITMSG";
+         const string shortMessage = "+Whatever static class";
+         const string longMessage = "This will provide the whatever and this and that";
+         const string longMessage2 = "and it continues onto the second line";
+         const string longMessage3 = "and it continues onto the THIRD line";
+
+         var contents = new[]
+         {
+            shortMessage,
+            longMessage,
+            longMessage2,
+            longMessage3,
+            "",
+            "# Please enter the commit message for your changes. Lines starting",
+            "# with '#' will be ignored, and an empty message aborts the commit.",
+            "# On branch feature/supportAmend",
+            "# Changes to be committed:",
+            "#	modified:   src/GitWrite/GitWrite.UnitTests/AppControllerTests.cs",
+            "#"
+         };
+
+         // Setup
+
+         var fileAdapterMock = new Mock<IFileAdapter>();
+         fileAdapterMock.Setup( fa => fa.Exists( path ) ).Returns( true );
+         fileAdapterMock.Setup( fa => fa.ReadAllLines( path ) ).Returns( contents );
+         SimpleIoc.Default.Register( () => fileAdapterMock.Object );
+
+         // Test
+
+         var commitFileReader = new CommitFileReader();
+
+         var commitDocument = commitFileReader.FromFile( path );
+
+         // Assert
+
+         Assert.AreEqual( shortMessage, commitDocument.ShortMessage );
+         Assert.AreEqual( longMessage, commitDocument.LongMessage[0] );
+         Assert.AreEqual( longMessage2, commitDocument.LongMessage[1] );
+         Assert.AreEqual( longMessage3, commitDocument.LongMessage[2] );
+      }
    }
 }
