9384f5c477d3c3033c3c940b211c17ae5652ef7b
Alex Novak <alexwnovak@gmail.com>
2015-11-02 08:12:52 -0500

    Using expression bodies for FileAdapter

--- src/GitWrite/GitWrite/FileAdapter.cs
+++ src/GitWrite/GitWrite/FileAdapter.cs
@@ -4,19 +4,10 @@ namespace GitWrite
 {
    public class FileAdapter : IFileAdapter
    {
-      public bool Exists( string path )
-      {
-         return File.Exists( path );
-      }
+      public bool Exists( string path ) => File.Exists( path );
 
-      public string[] ReadAllLines( string path )
-      {
-         return File.ReadAllLines( path );
-      }
+      public string[] ReadAllLines( string path ) => File.ReadAllLines( path );
 
-      public void WriteAllLines( string path, string[] lines )
-      {
-         File.WriteAllLines( path, lines );  
-      }
+      public void WriteAllLines( string path, string[] lines ) => File.WriteAllLines( path, lines );
    }
 }
