17ec8073a0b2d92a0f9dcf3867ac7728e49f28c9
Alex Novak <alexwnovak@gmail.com>
2015-11-01 21:37:51 -0500

    +File name constants for the kinds of incoming Git files

---
+++ src/GitWrite/GitWrite/GitFileNames.cs
@@ -0,0 +1,8 @@
+﻿namespace GitWrite
+{
+   public static class GitFileNames
+   {
+      public const string CommitFileName = "COMMIT_EDITMSG";
+      public const string InteractiveRebaseFileName = "git-rebase-todo";
+   }
+}
