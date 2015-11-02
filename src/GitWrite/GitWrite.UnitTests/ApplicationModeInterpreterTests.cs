5bf04109d659fd0f06ac3ee028db386e9f6ea26c
Alex Novak <alexwnovak@gmail.com>
2015-11-01 21:25:28 -0500

    Returns Unknown if passing null or empty string

--- src/GitWrite/GitWrite.UnitTests/ApplicationModeInterpreterTests.cs
+++ src/GitWrite/GitWrite.UnitTests/ApplicationModeInterpreterTests.cs
@@ -5,5 +5,20 @@ namespace GitWrite.UnitTests
    [TestClass]
    public class ApplicationModeInterpreterTests
    {
+      [TestMethod]
+      public void GetFromFileName_NullFileName_ReturnsUnknown()
+      {
+         var applicationMode = ApplicationModeInterpreter.GetFromFileName( null );
+
+         Assert.AreEqual( ApplicationMode.Unknown, applicationMode );
+      }
+
+      [TestMethod]
+      public void GetFromFileName_EmptyStringFileName_ReturnsUnknown()
+      {
+         var applicationMode = ApplicationModeInterpreter.GetFromFileName( string.Empty );
+
+         Assert.AreEqual( ApplicationMode.Unknown, applicationMode );
+      }
    }
 }
--- src/GitWrite/GitWrite/ApplicationModeInterpreter.cs
+++ src/GitWrite/GitWrite/ApplicationModeInterpreter.cs
@@ -2,5 +2,9 @@
 {
    public static class ApplicationModeInterpreter
    {
+      public static ApplicationMode GetFromFileName( string fileName )
+      {
+         return ApplicationMode.Unknown;
+      }
    }
 }
