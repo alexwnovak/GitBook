16426d595dea67c672306d6967b1bc3ee04583f7
Alex Novak <alexwnovak@gmail.com>
2015-11-02 07:45:56 -0500

    Refactor how commit documents are created
    Moved some method calls around to make it more readable. About to throw
    some more stuff in there.

--- src/GitWrite/GitWrite/CommitFileReader.cs
+++ src/GitWrite/GitWrite/CommitFileReader.cs
@@ -4,20 +4,32 @@ namespace GitWrite
 {
    public class CommitFileReader : ICommitFileReader
    {
+      private readonly IFileAdapter _fileAdapter = SimpleIoc.Default.GetInstance<IFileAdapter>();
+
       public CommitDocument FromFile( string path )
       {
-         var fileAdapter = SimpleIoc.Default.GetInstance<IFileAdapter>();
+         ThrowIfNotExists( path );
 
-         bool fileExists = fileAdapter.Exists( path );
+         var commitDocument = CreateBasicDocument( path );
+
+         return commitDocument;
+      }
+
+      private void ThrowIfNotExists( string path )
+      {
+         bool fileExists = _fileAdapter.Exists( path );
 
          if ( !fileExists )
          {
             throw new GitFileLoadException();
          }
+      }
 
+      private CommitDocument CreateBasicDocument( string path )
+      {
          return new CommitDocument
          {
-            InitialLines = fileAdapter.ReadAllLines( path ),
+            InitialLines = _fileAdapter.ReadAllLines( path ),
             Name = path
          };
       }
