c9b78351680ab1715bf3857668081569f7c5dd90
Alex Novak <alexwnovak@gmail.com>
2015-11-02 08:20:36 -0500

    +Test for parsing short and long message with a comment line between

--- src/GitWrite/GitWrite.UnitTests/CommitFileReaderTests.cs
+++ src/GitWrite/GitWrite.UnitTests/CommitFileReaderTests.cs
@@ -338,5 +338,88 @@ namespace GitWrite.UnitTests
          Assert.AreEqual( shortMessage, commitDocument.ShortMessage );
          Assert.AreEqual( longMessage, commitDocument.LongMessage );
       }
+
+      [TestMethod]
+      public void FromFile_FileHasLongMessageWithOneLineAndMiddleLineBreak_SetsShortAndLongMessage()
+      {
+         const string path = "COMMIT_EDITMSG";
+         const string shortMessage = "+Whatever static class";
+         const string longMessage = "This will provide the whatever and this and that";
+
+         var contents = new[]
+         {
+            shortMessage,
+            "",
+            longMessage,
+            "",
+            "# Please enter the commit message for your changes. Lines starting",
+            "# with '#' will be ignored, and an empty message aborts the commit.",
+            "# On branch feature/supportAmend",
+            "# Changes to be committed:",
+            "#	modified:   src/GitWrite/GitWrite.UnitTests/AppControllerTests.cs",
+            "#"
+         };
+
+         // Setup
+
+         var fileAdapterMock = new Mock<IFileAdapter>();
+         fileAdapterMock.Setup( fa => fa.Exists( path ) ).Returns( true );
+         fileAdapterMock.Setup( fa => fa.ReadAllLines( path ) ).Returns( contents );
+         SimpleIoc.Default.Register( () => fileAdapterMock.Object );
+
+         // Test
+
+         var commitFileReader = new CommitFileReader();
+
+         var commitDocument = commitFileReader.FromFile( path );
+
+         // Assert
+
+         Assert.AreEqual( shortMessage, commitDocument.ShortMessage );
+         Assert.AreEqual( longMessage, commitDocument.LongMessage );
+      }
+
+      [TestMethod]
+      public void FromFile_FileHasLongMessageWithOneLineAndCommentInBetween_SetsShortAndLongMessage()
+      {
+         const string path = "COMMIT_EDITMSG";
+         const string shortMessage = "+Whatever static class";
+         const string longMessage = "This will provide the whatever and this and that";
+
+         var contents = new[]
+         {
+            shortMessage,
+            "",
+            "# A comment between the messages, like from a conflict resolution",
+            "",
+            longMessage,
+            "",
+            "# Please enter the commit message for your changes. Lines starting",
+            "# with '#' will be ignored, and an empty message aborts the commit.",
+            "# On branch feature/supportAmend",
+            "# Changes to be committed:",
+            "#	modified:   src/GitWrite/GitWrite.UnitTests/AppControllerTests.cs",
+            "#"
+         };
+
+         // Setup
+
+         var fileAdapterMock = new Mock<IFileAdapter>();
+         fileAdapterMock.Setup( fa => fa.Exists( path ) ).Returns( true );
+         fileAdapterMock.Setup( fa => fa.ReadAllLines( path ) ).Returns( contents );
+         SimpleIoc.Default.Register( () => fileAdapterMock.Object );
+
+         // Test
+
+         var commitFileReader = new CommitFileReader();
+
+         var commitDocument = commitFileReader.FromFile( path );
+
+         // Assert
+
+         Assert.AreEqual( shortMessage, commitDocument.ShortMessage );
+         Assert.AreEqual( longMessage, commitDocument.LongMessage );
+      }
+
    }
 }
