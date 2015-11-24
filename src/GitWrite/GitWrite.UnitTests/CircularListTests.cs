b2732bd2ea2ea4c90bcd3ff6d7e3a2274d54855b
Alex Novak <alexwnovak@gmail.com>
2015-11-23 19:34:06 -0500

    +CircularList blank class
    This is like a list, except you can iterate through circularly, and run past the
    end element and wrap to the first (and vice versa).
    
    There will also be a way to "select" an item, and go forward and backward through it,
    rather than using an enumerator.

---
+++ src/GitWrite/GitWrite.UnitTests/CircularListTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitWrite.UnitTests
+{
+   [TestClass]
+   public class CircularListTests
+   {
+   }
+}
---
+++ src/GitWrite/GitWrite/CircularList.cs
@@ -0,0 +1,6 @@
+﻿namespace GitWrite
+{
+   public class CircularList<T>
+   {
+   }
+}
