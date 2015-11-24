f136decbf4cd4c8b0b5fb13d2086fd61b3c9658e
Alex Novak <alexwnovak@gmail.com>
2015-11-23 19:46:55 -0500

    +ListNode nested class
    An implementation detail to how CircularList will work; we'll use a linked list
    and use this small class to carry the data, while allowing forward and backward
    pointers.

--- src/GitWrite/GitWrite/CircularList.cs
+++ src/GitWrite/GitWrite/CircularList.cs
@@ -8,5 +8,40 @@ namespace GitWrite
       {
          throw new NotImplementedException();
       }
+
+      #region ListNode implementation
+
+      private class ListNode<TDataType>
+      {
+         public TDataType Value
+         {
+            get;
+         }
+
+         public ListNode<TDataType> Next
+         {
+            get;
+            set;
+         }
+
+         public ListNode<TDataType> Previous
+         {
+            get;
+            set;
+         }
+
+         public ListNode()
+         {
+         }
+
+         public ListNode( TDataType value )
+         {
+            Value = value;
+         }
+
+         public override string ToString() => Value.ToString();
+      }
+
+      #endregion
    }
 }
