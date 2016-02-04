dc39cf98c3c403f900194a1307f6523bcba62fa5
Alex Novak <alexwnovak@gmail.com>
2016-02-03 19:44:56 -0500

    Ditching the help strip at the bottom
    Can't make it look good, so it's coming out for now.

--- src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
+++ src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
@@ -12,7 +12,6 @@ namespace GitWrite.Views
       public CommitWindow()
       {
          InitializeComponent();
-         Height = 86;
 
          _viewModel = (CommitViewModel) DataContext;
          _viewModel.ExpansionRequested += OnExpansionRequested;
