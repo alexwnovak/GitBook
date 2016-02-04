082d43f232de6bfd0cc534ac341a5e2f4acfe855
Alex Novak <alexwnovak@gmail.com>
2016-02-03 20:59:58 -0500

    +IStoryboardHelper and blank concrete
    This guy will let us fire off a storyboard and await its result
    asynchronously, rather than fire-and-forgetting it.

---
+++ src/GitWrite/GitWrite/Views/StoryboardHelper.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+
+namespace GitWrite.Views
+{
+   public class StoryboardHelper : IStoryboardHelper
+   {
+      public Task PlayAsync( string name )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
