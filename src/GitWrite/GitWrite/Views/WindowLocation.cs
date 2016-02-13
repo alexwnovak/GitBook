9679b73aed07fb2930eb5cbc89ebba5b1d4b3e2d
Alex Novak <alexwnovak@gmail.com>
2016-02-13 12:46:00 -0500

    +WindowLocation static class for starting/ending Y window locations

---
+++ src/GitWrite/GitWrite/Views/WindowLocation.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace GitWrite.Views
+{
+   public static class WindowLocation
+   {
+      public static readonly double RestingPointY = 0.7 * ( SystemParameters.FullPrimaryScreenHeight - 30 ) / 2;
+      public static readonly double StartingPointY = RestingPointY + 20;
+   }
+}
