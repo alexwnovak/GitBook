4994de94f574b7b4935a6247b4aae3e9b317dc88
Alex Novak <alexwnovak@gmail.com>
2016-02-18 19:03:56 -0500

    Renaming WindowLocation -> WindowValues
    Since it's now more than just about location.

--- src/GitWrite/GitWrite/Views/WindowValues.cs
+++ src/GitWrite/GitWrite/Views/WindowValues.cs
@@ -2,12 +2,12 @@ using System.Windows;
 
 namespace GitWrite.Views
 {
-   public static class WindowLocation
+   public static class WindowValues
    {
       public static readonly double RestingPointY = 0.7 * ( SystemParameters.FullPrimaryScreenHeight - 30 ) / 2;
       public static readonly double StartingPointY = RestingPointY + 20;
 
       public static readonly double NonExpandedHeight = 76;
-      public static readonly double ExpandedHeight = 800;
+      public static readonly double ExpandedHeight = 400;
    }
 }
