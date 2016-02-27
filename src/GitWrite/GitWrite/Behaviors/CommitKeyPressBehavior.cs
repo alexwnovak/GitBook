5bf618a6a5af2a8b960d2148c795d960a0bb1f75
Alex Novak <alexwnovak@gmail.com>
2016-02-27 16:43:29 -0500

    Hooking up to the PreviewKeyDown event
    We'll use this to see what keys are happening.

--- src/GitWrite/GitWrite/Behaviors/CommitKeyPressBehavior.cs
+++ src/GitWrite/GitWrite/Behaviors/CommitKeyPressBehavior.cs
@@ -1,9 +1,17 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interactivity;
 
 namespace GitWrite.Behaviors
 {
    public class CommitKeyPressBehavior : Behavior<Window>
    {
+      protected override void OnAttached() => AssociatedObject.PreviewKeyDown += AssociatedObject_PreviewKeyDown;
+      protected override void OnDetaching() => AssociatedObject.PreviewKeyDown -= AssociatedObject_PreviewKeyDown;
+      
+      private void AssociatedObject_PreviewKeyDown( object sender, KeyEventArgs e )
+      {
+         
+      }
    }
 }
