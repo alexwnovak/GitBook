c8d5226477a91d3d71a5d2406d4ca1cee3d635df
Alex Novak <alexwnovak@gmail.com>
2016-02-26 21:55:00 -0500

    +Test for writing out the long message with an extra line break

--- src/GitWrite/GitWrite.UnitTests/CommitDocumentTests.cs
+++ src/GitWrite/GitWrite.UnitTests/CommitDocumentTests.cs
@@ -81,5 +81,39 @@ namespace GitWrite.UnitTests
 
          Assert.IsTrue( parametersMatch );
       }
+
+      [TestMethod]
+      public void Save_HasLongMessageInTwoParagraphs_WritesLongMessage()
+      {
+         bool parametersMatch = false;
+         const string path = "SomeFile.txt";
+         const string shortMessage = "This is the short message";
+         string longMessage = $"First line {Environment.NewLine}{Environment.NewLine} Second line after one blank line";
+
+         // Setup
+
+         var fileAdapterMock = new Mock<IFileAdapter>();
+         fileAdapterMock.Setup( fa => fa.WriteAllLines( It.IsAny<string>(), It.IsAny<IEnumerable<string>>() ) ).Callback<string, IEnumerable<string>>( ( p, l ) =>
+         {
+            var lines = l.ToList();
+            parametersMatch = ( p == path && lines[0] == shortMessage && lines[1] == longMessage );
+         } );
+         SimpleIoc.Default.Register( () => fileAdapterMock.Object );
+
+         // Test
+
+         var commitDocument = new CommitDocument
+         {
+            Name = path,
+            ShortMessage = shortMessage,
+            LongMessage = longMessage,
+         };
+
+         commitDocument.Save();
+
+         // Assert
+
+         Assert.IsTrue( parametersMatch );
+      }
    }
 }
