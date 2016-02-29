4f4527bcef5c465756194c34c57a2874d054899d
Alex Novak <alexwnovak@gmail.com>
2016-02-28 20:00:54 -0500

    +Generic form of the AsyncEventHandler that returns something
    This kind of goes against the usual form for event handler delegate types. I could've derived a new EventArgs class to let the event responder set the "args" instance to a result. This felt kinda weird, since you could just as easily not set a result. With a return type though, you HAVE to do something, which is clearly non-optional.

--- src/GitWrite/GitWrite/AsyncEventHandler.cs
+++ src/GitWrite/GitWrite/AsyncEventHandler.cs
@@ -4,4 +4,6 @@ using System.Threading.Tasks;
 namespace GitWrite
 {
    public delegate Task AsyncEventHandler( object sender, EventArgs e );
+
+   public delegate Task<T> AsyncEventHandler<T>( object sender, EventArgs e );
 }
