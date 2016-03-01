e4aa67e3b032c441257bf82109ac7f681cdd1586
Alex Novak <alexwnovak@gmail.com>
2016-02-29 20:11:01 -0500

    +PlayStoryboard extension method

--- src/GitWrite/GitWrite/FrameworkElementExtensions.cs
+++ src/GitWrite/GitWrite/FrameworkElementExtensions.cs
@@ -1,6 +1,11 @@
-﻿namespace GitWrite
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace GitWrite
 {
    public static class FrameworkElementExtensions
    {
+      public static void PlayStoryboard( this FrameworkElement frameworkElement, string storyboardName )
+         => ( frameworkElement.Resources[storyboardName] as Storyboard )?.Begin();
    }
 }
