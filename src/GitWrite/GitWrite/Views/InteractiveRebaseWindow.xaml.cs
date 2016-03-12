5326492c7096ad360e9a433e1b17d2d1c4746441
Alex Novak <alexwnovak@gmail.com>
2016-03-11 23:56:26 -0500

    Duping the CommitWindow UI, calling it InteractiveRebaseWindow
    This will be the UI for doing interactive rebases. Right now all the resources are duped, but can probably be unified more in the future. We're sketching out the skeleton here, so we'll get it running before we tune it.

---
+++ src/GitWrite/GitWrite/Views/InteractiveRebaseWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace GitWrite.Views
+{
+   public partial class InteractiveRebaseWindow : Window
+   {
+      public InteractiveRebaseWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
