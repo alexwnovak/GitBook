0886e23410e69ff9d9348a8e2d003cc256453459
Alex Novak <alexwnovak@gmail.com>
2016-03-12 00:00:54 -0500

    +Items collection with dummy data
    Dummy data as we get the UI wired together. In the future these will be the individual lines read from the Git file that lists all your commits.

--- src/GitWrite/GitWrite/ViewModels/InteractiveRebaseViewModel.cs
+++ src/GitWrite/GitWrite/ViewModels/InteractiveRebaseViewModel.cs
@@ -1,8 +1,23 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
 
 namespace GitWrite.ViewModels
 {
    public class InteractiveRebaseViewModel : ViewModelBase
    {
+      public ObservableCollection<RebaseItem> Items 
+      {
+         get;
+      }
+
+      public InteractiveRebaseViewModel()
+      {
+         Items = new ObservableCollection<RebaseItem>
+         {
+            new RebaseItem( "One" ),
+            new RebaseItem( "Two" ),
+            new RebaseItem( "Three" )
+         };
+      }
    }
 }
