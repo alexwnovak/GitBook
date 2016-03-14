862962d84e2001c672f5411566f398183508810f
Alex Novak <alexwnovak@gmail.com>
2016-03-13 21:26:36 -0400

    Up/Down arrows now move the selected item up and down

--- src/GitWrite/GitWrite/Views/Controls/InteractiveRebasePanel.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/InteractiveRebasePanel.xaml.cs
@@ -1,8 +1,31 @@
-using System.Windows;
+﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace GitWrite.Views.Controls
 {
    public partial class InteractiveRebasePanel : ListBox
    {
+      public InteractiveRebasePanel()
+      {
+         InitializeComponent();
+      }
+
+      private void InteractiveRebaseWindow_OnPreviewKeyDown( object sender, KeyEventArgs e )
+      {
+         if ( e.Key == Key.Down )
+         {
+            if ( SelectedIndex + 1 < Items.Count )
+            {
+               SelectedIndex++;
+            }
+         }
+         else if ( e.Key == Key.Up )
+         {
+            if ( SelectedIndex - 1 >= 0 )
+            {
+               SelectedIndex--;
+            }
+         }
+      }
    }
 }
