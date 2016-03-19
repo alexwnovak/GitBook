a3c6afceda769e2a3e25a0804c2b6ece52174ca4
Alex Novak <alexwnovak@gmail.com>
2016-03-19 16:44:14 -0400

    Implementations to read and write the selected theme name

--- src/GitWrite/GitWrite/Services/RegistryService.cs
+++ src/GitWrite/GitWrite/Services/RegistryService.cs
@@ -1,14 +1,31 @@
+﻿using Microsoft.Win32;
+
 namespace GitWrite.Services
 {
    public class RegistryService : IRegistryService
    {
       public string GetTheme()
       {
-         return null;
+         using ( var gitWriteKey = Registry.CurrentUser.CreateSubKey( @"SOFTWARE\GitWrite" ) )
+         {
+            string theme = (string) gitWriteKey.GetValue( "Theme" );
+
+            if ( string.IsNullOrEmpty( theme ) )
+            {
+               SetTheme( "Default" );
+               return "Default";
+            }
+
+            return theme;
+         }
       }
 
       public void SetTheme( string name )
       {
+         using ( var gitWriteKey = Registry.CurrentUser.CreateSubKey( @"SOFTWARE\GitWrite" ) )
+         {
+            gitWriteKey.SetValue( "Theme", name );
+         }
       }
    }
 }
