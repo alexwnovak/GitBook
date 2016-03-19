3008d75239191e9ce7cd0ceda1e22d71c4f19a37
Alex Novak <alexwnovak@gmail.com>
2016-03-19 14:20:11 -0400

    +Private Theme class that describes one theme
    Comes with a name and a set of colors. This is basically the set of colors we'll load in to replace one another.

--- src/GitWrite/GitWrite/Themes/ThemeSwitcher.cs
+++ src/GitWrite/GitWrite/Themes/ThemeSwitcher.cs
@@ -1,6 +1,34 @@
-﻿namespace GitWrite.Themes
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GitWrite.Themes
 {
    public static class ThemeSwitcher
    {
+      private class Theme
+      {
+         public string Name
+         {
+            get;
+         }
+
+         private readonly Dictionary<string, Brush> _colors = new Dictionary<string, Brush>();
+
+         public Theme( string name )
+         {
+            Name = name;
+         }
+
+         public void AddColor( string name, Brush brush ) => _colors[name] = brush;
+
+         public void Apply()
+         {
+            foreach ( var item in _colors )
+            {
+               Application.Current.Resources[item.Key] = _colors[item.Key];
+            }
+         }
+      }
    }
 }
