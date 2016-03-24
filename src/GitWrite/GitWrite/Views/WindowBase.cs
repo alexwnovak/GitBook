a3014104fe27673a95a826d2de605416fdc51c9f
Alex Novak <alexwnovak@gmail.com>
2016-03-24 19:51:17 -0400

    +WindowBase class
    This enables blur by default. Not sure what happens if you're not on Windows 10. To make this configurable, we should put a dependency property or something on it to make it toggable/bindable.

---
+++ src/GitWrite/GitWrite/Views/WindowBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GitWrite.Views.Dwm;
+
+namespace GitWrite.Views
+{
+   public class WindowBase : Window
+   {
+      public WindowBase()
+      {
+         Loaded += ( sender, e ) =>
+         {
+            WindowCompositionManager.EnableWindowBlur( this );
+         };
+      }
+   }
+}
