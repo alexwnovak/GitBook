b57df8977d1cd90dcf98817ff83220b247ab362c
Alex Novak <alexwnovak@gmail.com>
2016-03-24 19:15:30 -0400

    +AccentPolicy struct
    We'll pass this to the actual function to enable blur.

---
+++ src/GitWrite/GitWrite/Views/Dwm/AccentPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GitWrite.Views.Dwm
+{
+   [StructLayout( LayoutKind.Sequential )]
+   internal struct AccentPolicy
+   {
+      public AccentState AccentState;
+      public int AccentFlags;
+      public int GradientColor;
+      public int AnimationId;
+   }
+}
