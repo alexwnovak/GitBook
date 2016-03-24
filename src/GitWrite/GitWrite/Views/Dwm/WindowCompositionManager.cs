e424355e02747fb845df2338a9073cda39c96aec
Alex Novak <alexwnovak@gmail.com>
2016-03-24 19:18:53 -0400

    +WindowCompositionManager class
    This is what will actually enable the window blur. We'll call this, and it'll also hand off to the P/Invoke to make it happen.

---
+++ src/GitWrite/GitWrite/Views/Dwm/WindowCompositionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GitWrite.Views.Dwm
+{
+   internal static class WindowCompositionManager
+   {
+      [DllImport( "user32.dll" )]
+      private static extern int SetWindowCompositionAttribute( IntPtr hwnd, ref WindowCompositionAttributeData data );
+
+   }
+}
