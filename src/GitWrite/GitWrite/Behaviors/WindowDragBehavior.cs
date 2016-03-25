f8cd764804266a2e1f803857bb075691b27369f3
Alex Novak <alexwnovak@gmail.com>
2016-03-24 20:14:33 -0400

    Fleshing out the drag behavior
    - Left-clicking, dragging now moves the window around from its border
    - When you release, the final position is recorded into the registry

--- src/GitWrite/GitWrite/Behaviors/WindowDragBehavior.cs
+++ src/GitWrite/GitWrite/Behaviors/WindowDragBehavior.cs
@@ -1,8 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interactivity;
+using GalaSoft.MvvmLight.Ioc;
+using GitWrite.Services;
 
 namespace GitWrite.Behaviors
 {
    public class WindowDragBehavior : Behavior<Window>
    {
+      protected override void OnAttached()
+      {
+         AssociatedObject.MouseDown += OnMouseDown;
+      }
+      protected override void OnDetaching()
+      {
+         AssociatedObject.MouseDown -= OnMouseDown;
+      }
+
+      private void OnMouseDown( object sender, MouseButtonEventArgs e )
+      {
+         if ( e.LeftButton == MouseButtonState.Pressed )
+         {
+            AssociatedObject.DragMove();
+
+            var registryService = SimpleIoc.Default.GetInstance<IRegistryService>();
+
+            registryService.SetWindowX( (int) AssociatedObject.RestoreBounds.Left );
+            registryService.SetWindowY( (int) AssociatedObject.RestoreBounds.Top );
+         }
+      }
    }
 }
