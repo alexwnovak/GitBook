ab1d5cdfa273d5810eac47afac85eda3addaf50a
Alex Novak <alexwnovak@gmail.com>
2016-03-24 22:48:16 -0400

    Double-clicking the draggable border resets its position

--- src/GitWrite/GitWrite/Behaviors/WindowDragBehavior.cs
+++ src/GitWrite/GitWrite/Behaviors/WindowDragBehavior.cs
@@ -12,10 +12,12 @@ namespace GitWrite.Behaviors
       protected override void OnAttached()
       {
          AssociatedObject.MouseDown += OnMouseDown;
+         AssociatedObject.MouseDoubleClick += OnMouseDoubleClick;
       }
       protected override void OnDetaching()
       {
          AssociatedObject.MouseDown -= OnMouseDown;
+         AssociatedObject.MouseDoubleClick -= OnMouseDoubleClick;
       }
 
       private void OnMouseDown( object sender, MouseButtonEventArgs e )
@@ -29,6 +31,12 @@ namespace GitWrite.Behaviors
             registryService.SetWindowX( (int) AssociatedObject.RestoreBounds.Left );
             registryService.SetWindowY( (int) AssociatedObject.RestoreBounds.Top );
          }
+      }
+
+      private void OnMouseDoubleClick( object sender, MouseButtonEventArgs e )
+      {
+         AssociatedObject.Left = ( SystemParameters.FullPrimaryScreenWidth - AssociatedObject.Width ) / 2;
+         AssociatedObject.Top = 0.7 * ( SystemParameters.FullPrimaryScreenHeight - 30 ) / 2;
       }
    }
 }
