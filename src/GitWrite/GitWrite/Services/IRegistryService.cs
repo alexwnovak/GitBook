ff20abc0c31848a34129330d2f5998a85be1ef07
Alex Novak <alexwnovak@gmail.com>
2016-03-25 20:42:14 -0400

    +ReadString and WriteString methods to the registry service
    These will act more like general-purpose registry methods, instead of putting the GitWrite logic on top; that will be handled by another class on top of this one.

--- src/GitWrite/GitWrite/Services/IRegistryService.cs
+++ src/GitWrite/GitWrite/Services/IRegistryService.cs
@@ -1,7 +1,12 @@
-﻿namespace GitWrite.Services
+﻿using Microsoft.Win32;
+
+namespace GitWrite.Services
 {
    public interface IRegistryService
    {
+      string ReadString( RegistryKey registryKey, string path, string name );
+      void WriteString( RegistryKey registryKey, string path, string name, string value );
+
       string GetTheme();
 
       void SetTheme( string name );
--- src/GitWrite/GitWrite/Services/RegistryService.cs
+++ src/GitWrite/GitWrite/Services/RegistryService.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.Win32;
+﻿using System;
+using Microsoft.Win32;
 
 namespace GitWrite.Services
 {
    public class RegistryService : IRegistryService
    {
+      public string ReadString( RegistryKey registryKey, string path, string name )
+         => OpenKey( registryKey, path, k => (string) k.GetValue( name ) );
+
+      public void WriteString( RegistryKey registryKey, string path, string name, string value )
+         => OpenKey( registryKey, path, k => k.SetValue( name, value ) );
+
+      private static T OpenKey<T>( RegistryKey registryKey, string path, Func<RegistryKey, T> readAction )
+      {
+         using ( var key = registryKey.CreateSubKey( path ) )
+         {
+            return readAction( key );
+         }
+      }
+
+      private static void OpenKey( RegistryKey registryKey, string path, Action<RegistryKey> writeAction )
+      {
+         using ( var key = registryKey.CreateSubKey( path ) )
+         {
+            writeAction( key );
+         }
+      }
+
       public string GetTheme()
       {
          using ( var gitWriteKey = Registry.CurrentUser.CreateSubKey( @"SOFTWARE\GitWrite" ) )
