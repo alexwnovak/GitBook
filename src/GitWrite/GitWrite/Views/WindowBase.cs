d6849cf09c6840b2e418a617f34c53df0cbf19b7
Alex Novak <alexwnovak@gmail.com>
2016-03-28 21:26:56 -0400

    WindowBase now implements IViewService

--- src/GitWrite/GitWrite/Views/WindowBase.cs
+++ src/GitWrite/GitWrite/Views/WindowBase.cs
@@ -1,21 +1,28 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Interactivity;
+using GalaSoft.MvvmLight.Ioc;
 using GitWrite.Behaviors;
-using GitWrite.Views.Dwm;
+using GitWrite.Services;
+using GitWrite.Views.Controls;
 
 namespace GitWrite.Views
 {
-   public class WindowBase : Window
+   public class WindowBase : Window, IViewService
    {
       public WindowBase()
       {
-         Interaction.GetBehaviors( this ).Add( new WindowDragBehavior() );
-         Interaction.GetBehaviors( this ).Add( new WindowPlacementBehavior() );
+         SimpleIoc.Default.Register<IViewService>( () => this );
 
-         Loaded += ( sender, e ) =>
-         {
-            WindowCompositionManager.EnableWindowBlur( this );
-         };
+         var behaviors = Interaction.GetBehaviors( this );
+         behaviors.Add( new WindowDragBehavior() );
+         behaviors.Add( new WindowPlacementBehavior() );
+      }
+
+      public async Task<ConfirmationResult> ConfirmExitAsync()
+      {
+         throw new NotImplementedException();
       }
    }
 }
