1c96304408b59d1e94adb560cd27c36b169c1284
Alex Novak <alexwnovak@gmail.com>
2016-03-28 21:10:01 -0400

    +ShutdownRequested event
    This lets the view get notified when the view model needs the shutdown UI displayed.

--- src/GitWrite/GitWrite/ViewModels/GitWriteViewModelBase.cs
+++ src/GitWrite/GitWrite/ViewModels/GitWriteViewModelBase.cs
@@ -1,8 +1,12 @@
+﻿using System;
 using GalaSoft.MvvmLight;
 
 namespace GitWrite.ViewModels
 {
    public class GitWriteViewModelBase : ViewModelBase
    {
+      public event EventHandler<ShutdownEventArgs> ShutdownRequested;
+
+      protected virtual void OnShutdownRequested( object sender, ShutdownEventArgs e ) => ShutdownRequested?.Invoke( sender, e );
    }
 }
