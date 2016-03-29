2c1ef605204c8917db1b47209e17639f26769ea2
Alex Novak <alexwnovak@gmail.com>
2016-03-28 21:11:10 -0400

    Stubs for saving and discarding
    Subclasses will override these when they need to opt into specialized save and discard functionality.

--- src/GitWrite/GitWrite/ViewModels/GitWriteViewModelBase.cs
+++ src/GitWrite/GitWrite/ViewModels/GitWriteViewModelBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 
 namespace GitWrite.ViewModels
@@ -8,5 +9,15 @@ namespace GitWrite.ViewModels
       public event EventHandler<ShutdownEventArgs> ShutdownRequested;
 
       protected virtual void OnShutdownRequested( object sender, ShutdownEventArgs e ) => ShutdownRequested?.Invoke( sender, e );
+
+      protected virtual Task OnSaveAsync()
+      {
+         return Task.FromResult( true );
+      }
+
+      protected virtual Task OnDiscardAsync()
+      {
+         return Task.FromResult( true );
+      }
    }
 }
