1982c14f6ad066043b77b11eab5519c2c1d6b62d
Alex Novak <alexwnovak@gmail.com>
2016-04-09 12:30:21 -0400

    Trivial rejection tests with GetPath implementation

--- src/GitWrite/GitWrite.UnitTests/Services/GitRepositoryPathConverterTests.cs
+++ src/GitWrite/GitWrite.UnitTests/Services/GitRepositoryPathConverterTests.cs
@@ -1,6 +1,32 @@
-﻿namespace GitWrite.UnitTests.Services
+﻿using FluentAssertions;
+using GitWrite.Services;
+using Xunit;
+
+namespace GitWrite.UnitTests.Services
 {
    public class GitRepositoryPathConverterTests
    {
+      [Fact]
+      public void GetPath_CommitDocumentIsNull_ReturnsEmptyString()
+      {
+         string path = GitRepositoryPathConverter.GetPath( null );
+
+         path.Should().Be( string.Empty );
+      }
+
+      [Theory]
+      [InlineData( null, "" )]
+      [InlineData( "", "" )]
+      public void GetPath_DocumentNameVaries( string documentName, string expectedPath )
+      {
+         var commitDocument = new CommitDocument( null )
+         {
+            Name = documentName
+         };
+
+         string path = GitRepositoryPathConverter.GetPath( commitDocument );
+
+         path.Should().Be( expectedPath );
+      }
    }
 }
