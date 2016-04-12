d7e7cba4ba5de939c1bf048034d5cfabfc55f6de
Alex Novak <alexwnovak@gmail.com>
2016-04-11 20:09:42 -0400

    Now staging rebase document with some hard-coded items
    Placeholder until it gets hooked to a reader.

--- src/GitWrite/GitWrite/InteractiveRebaseDocument.cs
+++ src/GitWrite/GitWrite/InteractiveRebaseDocument.cs
@@ -1,6 +1,35 @@
-﻿namespace GitWrite
+﻿using GitWrite.ViewModels;
+
+namespace GitWrite
 {
    public class InteractiveRebaseDocument
    {
+      public string Name
+      {
+         get;
+         set;
+      }
+
+      public string[] RawLines
+      {
+         get;
+         set;
+      }
+
+      public RebaseItem[] RebaseItems
+      {
+         get;
+         set;
+      }
+
+      public InteractiveRebaseDocument()
+      {
+         RebaseItems = new[]
+         {
+            new RebaseItem( "One" ), 
+            new RebaseItem( "Two" ), 
+            new RebaseItem( "Three" ), 
+         };
+      }
    }
 }
