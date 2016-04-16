ff090ac7944ae201ab9809e199f0f0e16980e3d7
Alex Novak <alexwnovak@gmail.com>
2016-04-16 15:12:13 -0400

    +InteractiveRebasePanel
    This will be the new thing that contains the rebase items, but this
    time directly inherits ItemsControl, for a finer-grained control with
    less layers between the control and the data.

---
+++ src/GitWrite/GitWrite/Views/Controls/InteractiveRebasePanel.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace GitWrite.Views.Controls
+{
+   public partial class InteractiveRebasePanel : ItemsControl
+   {
+      public InteractiveRebasePanel()
+      {
+         InitializeComponent();
+      }
+   }
+}
