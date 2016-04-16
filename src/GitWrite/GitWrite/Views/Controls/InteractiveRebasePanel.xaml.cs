4e3c74a19acfe6f46d9b89b0fd596abf8a3f0876
Alex Novak <alexwnovak@gmail.com>
2016-04-16 15:47:19 -0400

    On load, grabs the scroll viewer and grid from the template
    We'll need to do stuff with them, so we stash references to the controls.

--- src/GitWrite/GitWrite/Views/Controls/InteractiveRebasePanel.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/InteractiveRebasePanel.xaml.cs
@@ -5,9 +5,18 @@ namespace GitWrite.Views.Controls
 {
    public partial class InteractiveRebasePanel : ItemsControl
    {
+      private ScrollViewer _scrollViewer;
+      private Grid _layoutGrid;
+
       public InteractiveRebasePanel()
       {
          InitializeComponent();
+      }
+
+      private void InteractiveRebasePanel_OnLoaded( object sender, RoutedEventArgs e )
+      {
+         _scrollViewer = (ScrollViewer) Template.FindName( "ScrollViewer", this );
+         _layoutGrid = (Grid) Template.FindName( "LayoutGrid", this );
       }
    }
 }
