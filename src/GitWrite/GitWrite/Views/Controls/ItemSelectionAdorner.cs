7bb31a7710af79ebb71fd35d2e03a26238eb295c
Alex Novak <alexwnovak@gmail.com>
2016-04-17 09:54:18 -0400

    Adorner now draws a rectangle over the entire adorned element

--- src/GitWrite/GitWrite/Views/Controls/ItemSelectionAdorner.cs
+++ src/GitWrite/GitWrite/Views/Controls/ItemSelectionAdorner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace GitWrite.Views.Controls
 {
@@ -8,6 +9,15 @@ namespace GitWrite.Views.Controls
       public ItemSelectionAdorner( UIElement adornedElement )
          : base( adornedElement )
       {
+      }
+
+      protected override void OnRender( DrawingContext drawingContext )
+      {
+         var size = AdornedElement.DesiredSize;
+         var rect = new Rect( new Point( 0, 0 ), new Point( ActualWidth, size.Height ) );
+
+         var brush = (Brush) Application.Current.Resources["HighlightColor"];
+         drawingContext.DrawRectangle( brush, null, rect );
       }
    }
 }
