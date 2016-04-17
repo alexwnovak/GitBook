8fac92074c58d5f0c2215b7b6d0d238393d941ca
Alex Novak <alexwnovak@gmail.com>
2016-04-17 09:51:49 -0400

    +ItemSelectionAdorner blank class
    This will be the thing that draws over selected items.

---
+++ src/GitWrite/GitWrite/Views/Controls/ItemSelectionAdorner.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace GitWrite.Views.Controls
+{
+   public class ItemSelectionAdorner : Adorner
+   {
+      public ItemSelectionAdorner( UIElement adornedElement )
+         : base( adornedElement )
+      {
+      }
+   }
+}
