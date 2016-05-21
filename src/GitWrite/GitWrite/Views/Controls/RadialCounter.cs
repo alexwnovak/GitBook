dc2070e57fb296013946744f2b135404b35fb6fb
Alex Novak <alexwnovak@gmail.com>
2016-05-21 15:45:25 -0400

    +Minimum and Maximum properties

--- src/GitWrite/GitWrite/Views/Controls/RadialCounter.cs
+++ src/GitWrite/GitWrite/Views/Controls/RadialCounter.cs
@@ -4,5 +4,38 @@ namespace GitWrite.Views.Controls
 {
    public class RadialCounter : FrameworkElement
    {
+      public static DependencyProperty MinimumProperty = DependencyProperty.Register( nameof( Minimum ),
+         typeof( int ),
+         typeof( RadialCounter ),
+         new FrameworkPropertyMetadata( 0, FrameworkPropertyMetadataOptions.AffectsRender ) );
+
+      public int Minimum
+      {
+         get
+         {
+            return (int) GetValue( MinimumProperty );
+         }
+         set
+         {
+            SetValue( MinimumProperty, value );
+         }
+      }
+
+      public static DependencyProperty MaximumProperty = DependencyProperty.Register( nameof( Maximum ),
+         typeof( int ),
+         typeof( RadialCounter ),
+         new FrameworkPropertyMetadata( 100, FrameworkPropertyMetadataOptions.AffectsRender ) );
+
+      public int Maximum
+      {
+         get
+         {
+            return (int) GetValue( MaximumProperty );
+         }
+         set
+         {
+            SetValue( MaximumProperty, value );
+         }
+      }
    }
 }
