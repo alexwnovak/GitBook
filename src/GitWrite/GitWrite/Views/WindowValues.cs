c6f8aa698dde314955b85b1ee74030dab7b3f1a3
Alex Novak <alexwnovak@gmail.com>
2016-05-21 22:39:48 -0400

    Adjusting the non-expanded height to 100
    This gives enough vertical space to accommodate the largest element (character counter), with enough breathing room for the drop shadow around it.

--- src/GitWrite/GitWrite/Views/WindowValues.cs
+++ src/GitWrite/GitWrite/Views/WindowValues.cs
@@ -7,7 +7,7 @@ namespace GitWrite.Views
       public static readonly double RestingPointY = 0.7 * ( SystemParameters.FullPrimaryScreenHeight - 30 ) / 2;
       public static readonly double StartingPointY = RestingPointY + 20;
 
-      public static readonly double NonExpandedHeight = 66;
+      public static readonly double NonExpandedHeight = 100;
       public static readonly double ExpandedHeight = 400;
    }
 }
