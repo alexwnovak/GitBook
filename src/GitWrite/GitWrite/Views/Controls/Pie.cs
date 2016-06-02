66ae18b2f8f0616717875c96dbb53f9e1998cbe1
Alex Novak <alexwnovak@gmail.com>
2016-06-02 12:21:49 -0400

    Pie now inherits Shape and provides a standard circle for its UI

--- src/GitWrite/GitWrite/Views/Controls/Pie.cs
+++ src/GitWrite/GitWrite/Views/Controls/Pie.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Windows.Shapes;
 
 namespace GitWrite.Views.Controls
 {
-   public class Pie
+   public class Pie : Shape
    {
+      protected override Geometry DefiningGeometry => CreateDefiningGeometry();
+
+      private Geometry CreateDefiningGeometry()
+      {
+         double halfWidth = Width / 2;
+         double halfHeight = Height / 2;
+
+         var centerPoint = new Point( halfWidth, halfHeight );
+         return new EllipseGeometry( centerPoint, halfWidth, halfHeight );
+      }
    }
 }
