e3325946528046636b6fa5ff0909a0fc702a4cad
Alex Novak <alexwnovak@gmail.com>
2016-06-02 12:24:36 -0400

    The clock-wipe is now driven by the Angle property
    Changing the Angle property will result in an update to the Pie's Clip property, which obscures/reveals as much of the circle as is dictated by the Angle. Meaning 0 will obscure everything (nothing showing); 360 will reveal everything (full circle); 180 will draw half the circle, from 12 o'clock to 6 o'clock, clockwise.

--- src/GitWrite/GitWrite/Views/Controls/Pie.cs
+++ src/GitWrite/GitWrite/Views/Controls/Pie.cs
@@ -10,7 +10,7 @@ namespace GitWrite.Views.Controls
       public static DependencyProperty AngleProperty = DependencyProperty.Register( nameof( Angle ),
          typeof( double ),
          typeof( Pie ),
-         new FrameworkPropertyMetadata( 0.0, FrameworkPropertyMetadataOptions.AffectsRender ) );
+         new FrameworkPropertyMetadata( 0.0, FrameworkPropertyMetadataOptions.AffectsRender, OnAngleChanged ) );
 
       public double Angle
       {
@@ -29,6 +29,11 @@ namespace GitWrite.Views.Controls
          Clip = new RectangleGeometry( Rect.Empty );
       }
 
+      private static void OnAngleChanged( DependencyObject obj, DependencyPropertyChangedEventArgs e )
+      {
+         (obj as Pie)?.UpdateClip();
+      }
+
       protected override Geometry DefiningGeometry => CreateDefiningGeometry();
 
       private Geometry CreateDefiningGeometry()
@@ -38,6 +43,23 @@ namespace GitWrite.Views.Controls
 
          var centerPoint = new Point( halfWidth, halfHeight );
          return new EllipseGeometry( centerPoint, halfWidth, halfHeight );
+      }
+
+      private void UpdateClip()
+      {
+         if ( Angle >= 360 )
+         {
+            Clip = CreateDefiningGeometry();
+            return;
+         }
+
+         double radius = Width / 2;
+         double radians = ( Angle - 90 ) * Math.PI / 180;
+
+         double x = radius * Math.Cos( radians ) + radius;
+         double y = radius * Math.Sin( radians ) + radius;
+
+         Clip = Geometry.Parse( $"M {radius},{radius} V 0 A {Width},{Height} 0 1 1 {x},{y}" );
       }
    }
 }
