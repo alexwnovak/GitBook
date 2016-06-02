f24f03aacd888cfe0eb7c0792f2f88e37f42503c
Alex Novak <alexwnovak@gmail.com>
2016-06-01 20:10:17 -0400

    +LeftClipConverter blank multi-converter
    We'll use this to "left clip" the primary commit notes. That is, it will be able to gradually mask (remove) parts of the text box from the left. This will support the exit animation, where it it's progressively hidden from left to right.

---
+++ src/GitWrite/GitWrite/Views/Converters/LeftClipConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GitWrite.Views.Converters
+{
+   public class LeftClipConverter : IMultiValueConverter
+   {
+      public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+
+      public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
