589609b5021944738ee6cd0c530a434846649572
Alex Novak <alexwnovak@gmail.com>
2016-06-11 13:19:08 -0400

    Can now issue a Save command to close the dialog with a "Save" result

--- src/GitWrite/GitWrite.UnitTests/ViewModels/ConfirmationViewModelTests.cs
+++ src/GitWrite/GitWrite.UnitTests/ViewModels/ConfirmationViewModelTests.cs
@@ -1,6 +1,23 @@
-﻿namespace GitWrite.UnitTests.ViewModels
+﻿using FluentAssertions;
+using GitWrite.ViewModels;
+using GitWrite.Views.Controls;
+using Xunit;
+
+namespace GitWrite.UnitTests.ViewModels
 {
    public class ConfirmationViewModelTests
    {
+      [Fact]
+      public void CloseRequested_RaisedFromSaveCommand_PassesSaveConfirmationResult()
+      {
+         ConfirmationResult actualResult = (ConfirmationResult) (-1);
+
+         var confirmationViewModel = new ConfirmationViewModel();
+         confirmationViewModel.CloseRequested += ( o, args ) => actualResult = args.ConfirmationResult;
+
+         confirmationViewModel.SaveCommand.Execute( null );
+
+         actualResult.Should().Be( ConfirmationResult.Save );
+      }
    }
 }
--- src/GitWrite/GitWrite/ViewModels/ConfirmationViewModel.cs
+++ src/GitWrite/GitWrite/ViewModels/ConfirmationViewModel.cs
@@ -1,8 +1,25 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GitWrite.Views.Controls;
 
 namespace GitWrite.ViewModels
 {
    public class ConfirmationViewModel : ViewModelBase
    {
+      public RelayCommand SaveCommand
+      {
+         get;
+      }
+
+      public event EventHandler<CloseRequestedEventArgs> CloseRequested; 
+
+      public ConfirmationViewModel()
+      {
+         SaveCommand = new RelayCommand( () => OnCloseRequested( this, new CloseRequestedEventArgs( ConfirmationResult.Save ) ) );;
+      }
+
+      protected virtual void OnCloseRequested( object sender, CloseRequestedEventArgs e )
+         => CloseRequested?.Invoke( sender, e );   
    }
 }
