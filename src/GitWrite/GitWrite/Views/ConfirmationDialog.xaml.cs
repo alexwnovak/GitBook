56d440b5a09b10f2c3677c8aebd25ec455f28b43
Alex Novak <alexwnovak@gmail.com>
2016-06-11 13:23:45 -0400

    ConfirmationDialog can now return a response from displaying
    Uses a new ShowDialog() to provide a ConfirmationResult, with some jockeying to commands in the view model to an event (with the result) back to the view.

--- src/GitWrite/GitWrite/Views/ConfirmationDialog.xaml.cs
+++ src/GitWrite/GitWrite/Views/ConfirmationDialog.xaml.cs
@@ -1,12 +1,34 @@
 ﻿using System.Windows;
+using GitWrite.ViewModels;
+using GitWrite.Views.Controls;
 
 namespace GitWrite.Views
 {
    public partial class ConfirmationDialog : Window
    {
+      private readonly ConfirmationViewModel _viewModel;
+      private ConfirmationResult _confirmationResult;
+
       public ConfirmationDialog()
       {
          InitializeComponent();
+
+         _viewModel = (ConfirmationViewModel) DataContext;
+         _viewModel.CloseRequested += OnCloseRequested;
+      }
+
+      public new ConfirmationResult ShowDialog()
+      {
+         base.ShowDialog();
+
+         return _confirmationResult;
+      }
+
+      private void OnCloseRequested( object sender, CloseRequestedEventArgs e )
+      {
+         _confirmationResult = e.ConfirmationResult;
+
+         Close();
       }
    }
 }
