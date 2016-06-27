b11d72e8a673975e0a32ea84261b4c9cec64844d
Alex Novak <alexwnovak@gmail.com>
2016-06-26 20:06:04 -0400

    Commenting out RadialCounter tests for now

--- src/GitWrite/GitWrite.UnitTests/Views/Controls/RadialCounterTests.cs
+++ src/GitWrite/GitWrite.UnitTests/Views/Controls/RadialCounterTests.cs
@@ -1,122 +1,122 @@
-﻿using FluentAssertions;
-using GitWrite.Services;
-using GitWrite.Views.Controls;
-using Moq;
-using Xunit;
+﻿//using FluentAssertions;
+//using GitWrite.Services;
+//using GitWrite.Views.Controls;
+//using Moq;
+//using Xunit;
 
-namespace GitWrite.UnitTests.Views.Controls
-{
-   public class RadialCounterTests
-   {
-      [StaFact]
-      public void Value_SetsValueLessThanMaximum_ValueSnapsToMinimum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//namespace GitWrite.UnitTests.Views.Controls
+//{
+//   public class RadialCounterTests
+//   {
+//      [StaFact]
+//      public void Value_SetsValueLessThanMaximum_ValueSnapsToMinimum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100,
-            Value = -20
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100,
+//            Value = -20
+//         };
 
-         radialCounter.Value.Should().Be( radialCounter.Minimum );
-      }
+//         radialCounter.Value.Should().Be( radialCounter.Minimum );
+//      }
 
-      [StaFact]
-      public void Value_SetsValueGreaterThanMaximum_ValueSnapsToMaximum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Value_SetsValueGreaterThanMaximum_ValueSnapsToMaximum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100,
-            Value = 123
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100,
+//            Value = 123
+//         };
 
-         radialCounter.Value.Should().Be( radialCounter.Maximum );
-      }
+//         radialCounter.Value.Should().Be( radialCounter.Maximum );
+//      }
 
-      [StaFact]
-      public void Value_SetsValueBetweenMinimumAndMaximum_ValueIsSet()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Value_SetsValueBetweenMinimumAndMaximum_ValueIsSet()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100,
-            Value = 66
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100,
+//            Value = 66
+//         };
 
-         radialCounter.Value.Should().Be( 66 );
-      }
+//         radialCounter.Value.Should().Be( 66 );
+//      }
 
-      [StaFact]
-      public void Value_MinimumBecomesGreaterThanCurrentValue_CurrentValueBecomesTheMinimum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Value_MinimumBecomesGreaterThanCurrentValue_CurrentValueBecomesTheMinimum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100,
-            Value = 25
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100,
+//            Value = 25
+//         };
 
-         radialCounter.Minimum = 50;
+//         radialCounter.Minimum = 50;
 
-         radialCounter.Value.Should().Be( radialCounter.Minimum );
-      }
+//         radialCounter.Value.Should().Be( radialCounter.Minimum );
+//      }
 
-      [StaFact]
-      public void Value_MaximumBecomesLessThanCurrentValue_CurrentValueBecomesTheMaximum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Value_MaximumBecomesLessThanCurrentValue_CurrentValueBecomesTheMaximum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100,
-            Value = 75
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100,
+//            Value = 75
+//         };
 
-         radialCounter.Maximum = 50;
+//         radialCounter.Maximum = 50;
 
-         radialCounter.Value.Should().Be( radialCounter.Maximum );
-      }
+//         radialCounter.Value.Should().Be( radialCounter.Maximum );
+//      }
 
-      [StaFact]
-      public void Minimum_MinimumIsSetGreaterThanMaximum_MinimumIsClampedEqualToMaximum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Minimum_MinimumIsSetGreaterThanMaximum_MinimumIsClampedEqualToMaximum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 0,
-            Maximum = 100
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 0,
+//            Maximum = 100
+//         };
 
-         radialCounter.Minimum = 125;
+//         radialCounter.Minimum = 125;
 
-         radialCounter.Minimum.Should().Be( radialCounter.Maximum );
-      }
+//         radialCounter.Minimum.Should().Be( radialCounter.Maximum );
+//      }
 
-      [StaFact]
-      public void Maximum_MaximumIsSetLessThanMinimum_MaximumIsClampedEqualToMinimum()
-      {
-         var resourceLocatorMock = new Mock<IResourceLocator>();
+//      [StaFact]
+//      public void Maximum_MaximumIsSetLessThanMinimum_MaximumIsClampedEqualToMinimum()
+//      {
+//         var resourceLocatorMock = new Mock<IResourceLocator>();
 
-         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
-         {
-            Minimum = 25,
-            Maximum = 100
-         };
+//         var radialCounter = new RadialCounter( resourceLocatorMock.Object )
+//         {
+//            Minimum = 25,
+//            Maximum = 100
+//         };
 
-         radialCounter.Maximum = 0;
+//         radialCounter.Maximum = 0;
 
-         radialCounter.Maximum.Should().Be( radialCounter.Minimum );
-      }
-   }
-}
+//         radialCounter.Maximum.Should().Be( radialCounter.Minimum );
+//      }
+//   }
+//}
