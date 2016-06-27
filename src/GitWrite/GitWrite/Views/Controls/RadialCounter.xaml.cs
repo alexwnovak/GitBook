d39b2c77d45f0323dd85385985a41b360c0a6648
Alex Novak <alexwnovak@gmail.com>
2016-06-26 20:49:21 -0400

    +Value dependency property with null callbacks for now

--- src/GitWrite/GitWrite/Views/Controls/RadialCounter.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/RadialCounter.xaml.cs
@@ -1,9 +1,27 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace GitWrite.Views.Controls
 {
    public partial class RadialCounter : UserControl
    {
+      public static DependencyProperty ValueProperty = DependencyProperty.Register( nameof( Value ),
+         typeof( int ),
+         typeof( RadialCounter ),
+         new FrameworkPropertyMetadata( 0, FrameworkPropertyMetadataOptions.AffectsRender, null ) );
+
+      public int Value
+      {
+         get
+         {
+            return (int) GetValue( ValueProperty );
+         }
+         set
+         {
+            SetValue( ValueProperty, value );
+         }
+      }
+
       public RadialCounter()
       {
          InitializeComponent();
