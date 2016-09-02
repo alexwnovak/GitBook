3b6433c9e3b3babb4c877e1c01a1cb187b9e3a31
Alex Novak <alexwnovak@gmail.com>
2016-09-01 21:50:44 -0400

    +Text property to relay the control's text box value outside itself
    It binds it to itself using its layout root as the DataContext so data binding isn't screwed up when the control gets used in turn. This allows the consumer to bind this new Text property to view models.

--- src/GitWrite/GitWrite/Views/Controls/MainEntryBox.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/MainEntryBox.xaml.cs
@@ -1,12 +1,37 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace GitWrite.Views.Controls
 {
    public partial class MainEntryBox : UserControl
    {
+      public static DependencyProperty TextProperty = DependencyProperty.Register( nameof( Text ),
+         typeof( string ),
+         typeof( MainEntryBox ),
+         new FrameworkPropertyMetadata( string.Empty,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            null,
+            null,
+            false,
+            UpdateSourceTrigger.PropertyChanged ) );
+
+      public string Text
+      {
+         get
+         {
+            return (string) GetValue( TextProperty );
+         }
+         set
+         {
+            SetValue( TextProperty, value );
+         }
+      }
+
       public MainEntryBox()
       {
          InitializeComponent();
+         LayoutRoot.DataContext = this;
       }
    }
 }
