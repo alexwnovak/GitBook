e60b439aff524013c449bfc574843aaf7dfa8e49
Alex Novak <alexwnovak@gmail.com>
2016-09-01 21:11:47 -0400

    +MainEntryBox blank control
    We'll pull out the top part of the UI into its own thing. This is the primary text box with the circle. We'll want to be able to instantiate it and transform it as an image to easily paint it onto the 3D version during the flippy animation.

---
+++ src/GitWrite/GitWrite/Views/Controls/MainEntryBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GitWrite.Views.Controls
+{
+   public partial class MainEntryBox : UserControl
+   {
+      public MainEntryBox()
+      {
+         InitializeComponent();
+      }
+   }
+}
