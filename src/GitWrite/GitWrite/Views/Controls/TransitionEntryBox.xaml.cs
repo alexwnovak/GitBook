f2c04d2b393b7fd6f3eda2c9a336fd28b8eb8686
Alex Novak <alexwnovak@gmail.com>
2016-09-03 17:08:18 -0400

    +TransitionEntryBox blank user control
    Instead of trying to permute the existing entry box, we'll actually use this stripped-down version and make it look the same, except we'll apply a different visual style. We can probably reuse the main one somehow, but this gets me closer to what I want faster.

---
+++ src/GitWrite/GitWrite/Views/Controls/TransitionEntryBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GitWrite.Views.Controls
+{
+   public partial class TransitionEntryBox : UserControl
+   {
+      public TransitionEntryBox()
+      {
+         InitializeComponent();
+      }
+   }
+}
