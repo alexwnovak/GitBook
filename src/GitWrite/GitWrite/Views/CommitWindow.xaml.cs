5a6a6c208d4109d8c9f917a085172be70c28dc1e
Alex Novak <alexwnovak@gmail.com>
2016-09-05 22:29:52 -0400

    -Unnecessary using directive

--- src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
+++ src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using GitWrite.ViewModels;
-using GitWrite.Views.Controls;
 
 namespace GitWrite.Views
 {
