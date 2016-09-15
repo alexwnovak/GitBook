4f29f98406d03f029038ccc59652154ff6a84172
Alex Novak <alexwnovak@gmail.com>
2016-09-15 15:20:49 -0400

    +RadialTextBlock blank user control
    This will replace the RadialControl. What we really need is a flexible way to animate this thing, so less hand-drawing needs to be done. Need to do more with XAML.

---
+++ src/GitWrite/GitWrite/Views/Controls/RadialTextBlock.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GitWrite.Views.Controls
+{
+   public partial class RadialTextBlock : UserControl
+   {
+      public RadialTextBlock()
+      {
+         InitializeComponent();
+      }
+   }
+}
