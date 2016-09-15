797ade72be1a97292681a88e143bed4b2d6a95cc
Alex Novak <alexwnovak@gmail.com>
2016-09-15 19:47:46 -0400

    RadialTextBlock now has a Text dependency property
    AddOwner'd from the primary owner, MainEntryBox.

--- src/GitWrite/GitWrite/Views/Controls/RadialTextBlock.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/RadialTextBlock.xaml.cs
@@ -1,9 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace GitWrite.Views.Controls
 {
    public partial class RadialTextBlock : UserControl
    {
+      public static DependencyProperty TextProperty = MainEntryBox.TextProperty.AddOwner( typeof( RadialTextBlock ) );
+
+      public string Text
+      {
+         get
+         {
+            return (string) GetValue( TextProperty );
+         }
+         set
+         {
+            SetValue( TextProperty, value );
+         }
+      }
+
       public RadialTextBlock()
       {
          InitializeComponent();
