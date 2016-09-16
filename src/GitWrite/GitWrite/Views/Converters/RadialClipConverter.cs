c7b8a57667b4df877c17f1f479fd24f2e599b56a
Alex Novak <alexwnovak@gmail.com>
2016-09-16 12:34:19 -0400

    Fleshing out the radial clip converter
    Borrowed from an older implementation around the green pie thing.

--- src/GitWrite/GitWrite/Views/Converters/RadialClipConverter.cs
+++ src/GitWrite/GitWrite/Views/Converters/RadialClipConverter.cs
@@ -8,9 +8,39 @@ namespace GitWrite.Views.Converters
 {
    public class RadialClipConverter : IMultiValueConverter
    {
+      private Geometry CreateWholeClip( double radius )
+      {
+         var centerPoint = new Point( radius, radius );
+         return new EllipseGeometry( centerPoint, radius, radius );
+      }
+
       public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
       {
-         throw new NotImplementedException();
+         double size = (double) values[0];
+         double progress = (double) values[1];
+
+         double radius = size / 2;
+         double angle = 360 * progress;
+
+         Geometry clipGeometry;
+
+         if ( angle >= 360 )
+         {
+            clipGeometry = CreateWholeClip( radius );
+            clipGeometry.Freeze();
+
+            return clipGeometry;
+         }
+
+         double radians = ( angle - 90 ) * Math.PI / 180;
+         
+         double x = radius * Math.Cos( radians ) + radius;
+         double y = radius * Math.Sin( radians ) + radius;
+
+         clipGeometry = Geometry.Parse( $"M {radius},{radius} V 0 A {size},{size} 0 1 1 {x},{y}" );
+         clipGeometry.Freeze();
+
+         return clipGeometry;
       }
 
       public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
