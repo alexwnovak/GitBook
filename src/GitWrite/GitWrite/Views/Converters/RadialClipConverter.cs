a59b99e1b25c1289c784afbfd05b934bbb1b8f05
Alex Novak <alexwnovak@gmail.com>
2016-09-15 22:54:40 -0400

    +RadialClipConverter blank converter
    This is sort of like what we used to have with the green pie. We'll use it to clip the ellipse and simulate a sweeping arc in the progress ring.

---
+++ src/GitWrite/GitWrite/Views/Converters/RadialClipConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GitWrite.Views.Converters
+{
+   public class RadialClipConverter : IMultiValueConverter
+   {
+      public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+
+      public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
