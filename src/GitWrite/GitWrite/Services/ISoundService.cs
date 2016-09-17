cf64979acfe6c6fda5ea5c8eea06c2a032ce21be
Alex Novak <alexwnovak@gmail.com>
2016-09-17 18:10:21 -0400

    +PlayPopSound method
    This internally uses a SoundPlayer to play the .wav. Used this instead of a MediaPlayer or something, since those guys didn't have a good way to ingest the data (from the resource stream). This allowed the .wav to be an in-binary resource.

--- src/GitWrite/GitWrite/Services/ISoundService.cs
+++ src/GitWrite/GitWrite/Services/ISoundService.cs
@@ -2,5 +2,6 @@
 {
    public interface ISoundService
    {
+      void PlayPopSound();
    }
 }
--- src/GitWrite/GitWrite/Services/SoundService.cs
+++ src/GitWrite/GitWrite/Services/SoundService.cs
@@ -1,6 +1,18 @@
-﻿namespace GitWrite.Services
+﻿using System.Media;
+using GitWrite.Properties;
+
+namespace GitWrite.Services
 {
    public class SoundService : ISoundService
    {
+      public void PlayPopSound()
+      {
+         var soundPlayer = new SoundPlayer
+         {
+            Stream = Resources.PopSound
+         };
+
+         soundPlayer.Play();
+      }
    }
 }
