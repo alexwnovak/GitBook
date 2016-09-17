981803697ac9a0267ea61a1a86387661b9aab47d
Alex Novak <alexwnovak@gmail.com>
2016-09-17 15:31:05 -0400

    +Helper methods for shader creation, pack URI

--- src/GitWrite/GitWrite/Effects/EffectHelper.cs
+++ src/GitWrite/GitWrite/Effects/EffectHelper.cs
@@ -1,6 +1,27 @@
-﻿namespace GitWrite.Effects
+﻿using System;
+using System.Reflection;
+using System.Windows.Media.Effects;
+
+namespace GitWrite.Effects
 {
    public static class EffectHelper
    {
+      public static PixelShader CreateShader( string path )
+      {
+         return new PixelShader
+         {
+            UriSource = MakePackUri( path )
+         };
+      }
+
+      public static Uri MakePackUri( string relativeFile )
+      {
+         Assembly assembly = typeof( EffectHelper ).Assembly;
+
+         string assemblyShortName = assembly.ToString().Split( ',' )[0];
+         string uriString = $"pack://application:,,,/{assemblyShortName};component/{relativeFile}";
+
+         return new Uri( uriString );
+      }
    }
 }
