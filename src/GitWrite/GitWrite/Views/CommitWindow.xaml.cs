090e815567519ba2d076bf7e82853514969a449f
Alex Novak <alexwnovak@gmail.com>
2016-09-17 16:27:37 -0400

    Now progressively blurs out on exit

--- src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
+++ src/GitWrite/GitWrite/Views/CommitWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Media3D;
 using GitWrite.ViewModels;
 using Resx = GitWrite.Properties.Resources;
@@ -15,6 +16,7 @@ namespace GitWrite.Views
    public partial class CommitWindow : WindowBase
    {
       private readonly CommitViewModel _viewModel;
+      private readonly BlurEffect _blurEffect;
 
       public CommitWindow()
       {
@@ -25,6 +27,14 @@ namespace GitWrite.Views
          _viewModel.CollapseRequested += OnCollapseRequested;
          _viewModel.ShakeRequested += OnShakeRequested;
          _viewModel.AsyncExitRequested += OnAsyncExitRequested;
+
+         _blurEffect = new BlurEffect
+         {
+            Radius = 0
+         };
+
+         Viewport.Effect = _blurEffect;
+         RegisterName( "BlurEffect", _blurEffect );
       }
 
       private void CommitWindow_OnLoaded( object sender, RoutedEventArgs e )
@@ -125,6 +135,15 @@ namespace GitWrite.Views
             BeginTime = TimeSpan.FromMilliseconds( 700 )
          };
 
+         var blurRadiusAnimation = new DoubleAnimation( 0, 6, opacityAnimation.Duration )
+         {
+            BeginTime = opacityAnimation.BeginTime,
+            EasingFunction = new CircleEase
+            {
+               EasingMode = EasingMode.EaseOut
+            }
+         };
+
          var storyboard = new Storyboard();
          Storyboard.SetTargetName( rotationAnimation, "RotationTransform" );
          Storyboard.SetTargetProperty( rotationAnimation, new PropertyPath( AxisAngleRotation3D.AngleProperty ) );
@@ -132,8 +151,12 @@ namespace GitWrite.Views
          Storyboard.SetTarget( opacityAnimation, MainGrid );
          Storyboard.SetTargetProperty( opacityAnimation, new PropertyPath( OpacityProperty ) );
 
+         Storyboard.SetTargetName( blurRadiusAnimation, "BlurEffect" );
+         Storyboard.SetTargetProperty( blurRadiusAnimation, new PropertyPath( BlurEffect.RadiusProperty ) );
+
          storyboard.Children.Add( rotationAnimation );
          storyboard.Children.Add( opacityAnimation );
+         storyboard.Children.Add( blurRadiusAnimation );
 
          storyboard.Completed += ( _, __ ) =>
          {
