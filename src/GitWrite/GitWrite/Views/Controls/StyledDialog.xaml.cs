c3d22c2d4a33415cd8a92819bd857a9900ee266b
Alex Novak <alexwnovak@gmail.com>
2016-09-23 23:51:47 -0400

    Can now set up buttons
    It's pretty dumb, and creates button objects based on the enum value.

--- src/GitWrite/GitWrite/Views/Controls/StyledDialog.xaml.cs
+++ src/GitWrite/GitWrite/Views/Controls/StyledDialog.xaml.cs
@@ -16,6 +16,72 @@ namespace GitWrite.Views.Controls
          InitializeComponent();
       }
 
+      private void SetupButtons( DialogButtons buttons )
+      {
+         switch ( buttons )
+         {
+            case DialogButtons.OK:
+            {
+               CreateButton( Resx.OKText );
+               break;
+            }
+            case DialogButtons.YesNo:
+            {
+               CreateButton( Resx.YesText );
+               CreateButton( Resx.NoText );
+               break;
+            }
+            case DialogButtons.YesNoCancel:
+            {
+               CreateButton( Resx.YesText );
+               CreateButton( Resx.NoText );
+               CreateButton( Resx.CancelText );
+               break;
+            }
+            case DialogButtons.SaveDiscardCancel:
+            {
+               CreateButton( Resx.SaveText );
+               CreateButton( Resx.DiscardText );
+               CreateButton( Resx.CancelText );
+               break;
+            }
+         }
+      }
+
+      private void CreateButton( string text )
+      {
+         var button = new Button
+         {
+            Height = 30,
+            HorizontalAlignment = HorizontalAlignment.Right,
+            Margin = new Thickness( 0, 0, 6, 0 ),
+            VerticalAlignment = VerticalAlignment.Bottom,
+            Width = 80,
+         };
+
+         button.Click += Button_OnClick;
+
+         var grid = new Grid
+         {
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            VerticalAlignment = VerticalAlignment.Stretch
+         };
+
+         var accessText = new AccessText
+         {
+            Foreground = Brushes.White,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
+            Text = text
+         };
+
+         grid.Children.Add( accessText );
+
+         button.Content = grid;
+
+         ButtonPanel.Children.Add( button );
+      }
+
       private void Button_OnClick( object sender, EventArgs e )
       {
          var button = (Button) sender;
