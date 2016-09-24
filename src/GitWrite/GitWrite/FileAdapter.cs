d341d083d7fb60a8e135a06ed3a4cc1bb29f7570
Alex Novak <alexwnovak@gmail.com>
2016-09-24 11:12:05 -0400

    +GetFileSize method for IFileAdapter

--- src/GitWrite/GitWrite/FileAdapter.cs
+++ src/GitWrite/GitWrite/FileAdapter.cs
@@ -7,6 +7,8 @@ namespace GitWrite
    {
       public bool Exists( string path ) => File.Exists( path );
 
+      public long GetFileSize( string path ) => new FileInfo( path ).Length;
+
       public string[] ReadAllLines( string path ) => File.ReadAllLines( path );
 
       public void WriteAllLines( string path, IEnumerable<string> lines ) => File.WriteAllLines( path, lines );
