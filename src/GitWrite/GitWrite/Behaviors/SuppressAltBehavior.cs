e61db45d486f18adbc0c7404c3966311aad47c56
Alex Novak <alexwnovak@gmail.com>
2016-09-26 19:04:08 -0400

    Behavior now uses the logic controller for the "handled" flag
    If the logic controller returns "true" to suppress, this means the UI calls it "handled," so it doesn't go any further and activate the Alt state.

--- src/GitWrite/GitWrite/Behaviors/SuppressAltBehavior.cs
+++ src/GitWrite/GitWrite/Behaviors/SuppressAltBehavior.cs
@@ -5,5 +5,16 @@ namespace GitWrite.Behaviors
 {
    public class SuppressAltBehavior : Behavior<Window>
    {
+      private readonly SuppressAltLogic _logicController;
+
+      public SuppressAltBehavior()
+      {
+         _logicController = new SuppressAltLogic();
+      }
+
+      protected override void OnAttached()
+      {
+         AssociatedObject.PreviewKeyDown += ( _, e ) => e.Handled = _logicController.ShouldSuppress( e.SystemKey );
+      }
    }
 }
