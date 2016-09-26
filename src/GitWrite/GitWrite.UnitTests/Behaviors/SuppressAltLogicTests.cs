8a0a0789b8a2f890edf08bb880ff02cfb646ea47
Alex Novak <alexwnovak@gmail.com>
2016-09-26 19:03:48 -0400

    Logic controller now knows to suppress if it's an Alt key

--- src/GitWrite/GitWrite.UnitTests/Behaviors/SuppressAltLogicTests.cs
+++ src/GitWrite/GitWrite.UnitTests/Behaviors/SuppressAltLogicTests.cs
@@ -1,6 +1,30 @@
-﻿namespace GitWrite.UnitTests.Behaviors
+﻿using System.Windows.Input;
+using FluentAssertions;
+using Xunit;
+using GitWrite.Behaviors;
+
+namespace GitWrite.UnitTests.Behaviors
 {
    public class SuppressAltLogicTests
    {
+      [Fact]
+      public void ShouldSuppress_KeyIsLeftAlt_Suppresses()
+      {
+         var logicController = new SuppressAltLogic();
+
+         bool shouldSuppress = logicController.ShouldSuppress( Key.LeftAlt );
+
+         shouldSuppress.Should().BeTrue();
+      }
+
+      [Fact]
+      public void ShouldSuppress_KeyIsRightAlt_Suppresses()
+      {
+         var logicController = new SuppressAltLogic();
+
+         bool shouldSuppress = logicController.ShouldSuppress( Key.RightAlt );
+
+         shouldSuppress.Should().BeTrue();
+      }
    }
 }
--- src/GitWrite/GitWrite/Behaviors/SuppressAltLogic.cs
+++ src/GitWrite/GitWrite/Behaviors/SuppressAltLogic.cs
@@ -1,6 +1,9 @@
+﻿using System.Windows.Input;
+
 namespace GitWrite.Behaviors
 {
    public class SuppressAltLogic
    {
+      public bool ShouldSuppress( Key key ) => key == Key.LeftAlt || key == Key.RightAlt;
    }
 }
