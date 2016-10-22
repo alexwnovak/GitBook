adcf5730f3c1fb1daacb8a35d9f68bdd2eb22ba8
Alex Novak <alexwnovak@gmail.com>
2016-10-22 14:07:08 -0400

    Fleshing out the behavior to select the triple-clicked line

--- src/GitWrite/GitWrite/Views/TextBoxExtensions.cs
+++ src/GitWrite/GitWrite/Views/TextBoxExtensions.cs
@@ -5,5 +5,11 @@ namespace GitWrite.Views
    public static class TextBoxExtensions
    {
       public static void MoveCaretToEnd( this TextBox textBox ) => textBox.SelectionStart = textBox.Text.Length;
+
+      public static void HighlightLine( this TextBox textBox, int lineIndex )
+      {
+         textBox.SelectionStart = textBox.GetCharacterIndexFromLineIndex( lineIndex );
+         textBox.SelectionLength = textBox.GetLineLength( lineIndex );
+      }
    }
 }
