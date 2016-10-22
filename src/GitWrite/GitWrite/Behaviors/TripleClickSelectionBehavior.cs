60441d5c5b1fd2d2360f6b768646cefa1579db20
Alex Novak <alexwnovak@gmail.com>
2016-10-22 10:55:00 -0400

    +TripleClickSelectionBehavior blank class
    This will be the thing that attaches to text boxes and allows whole lines to be selected by triple clicking.

---
+++ src/GitWrite/GitWrite/Behaviors/TripleClickSelectionBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace GitWrite.Behaviors
+{
+   public class TripleClickSelectionBehavior : Behavior<TextBox>
+   {
+   }
+}
