714603aab9314f588bd9b959a9fe0cf86b65ea4e
Alex Novak <alexwnovak@gmail.com>
2016-10-25 22:17:15 -0400

    Property now hangs onto a DependencyProperty instance

--- src/GitWrite/GitWrite/Animation/Property.cs
+++ src/GitWrite/GitWrite/Animation/Property.cs
@@ -1,6 +1,19 @@
+﻿using System.Windows;
+
 namespace GitWrite.Animation
 {
    public class Property
    {
+      public static Property Height = new Property( FrameworkElement.HeightProperty );
+
+      internal DependencyProperty DependencyProperty
+      {
+         get;
+      }
+
+      private Property( DependencyProperty dependencyProperty )
+      {
+         DependencyProperty = dependencyProperty;
+      }
    }
 }
