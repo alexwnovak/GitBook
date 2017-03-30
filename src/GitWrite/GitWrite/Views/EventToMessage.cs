7300f78ec5901a24c8d41745fb14d9704c2c5739
Alex Novak <alexwnovak@gmail.com>
2017-03-29 22:14:07 -0400

    +EventToMessage blank trigger action
    This is like EventToCommand, except it broadcasts a message instead of binding to a command. It's still used with the EventTrigger parent, making it possible to automatically send messages on events.

---
+++ src/GitWrite/GitWrite/Views/EventToMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace GitWrite.Views
+{
+   public class EventToMessage : TriggerAction<DependencyObject>
+   {
+      protected override void Invoke( object parameter )
+      {
+      }
+   }
+}
