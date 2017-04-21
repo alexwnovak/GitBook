69e6c6269f81633863f36adc47fb8f65d0f003a3
Alex Novak <alexwnovak@gmail.com>
2017-04-20 23:12:24 -0400

    Fixing typo for the view model

--- src/GitWrite/GitWrite/ViewModels/ViewModelLocator.cs
+++ src/GitWrite/GitWrite/ViewModels/ViewModelLocator.cs
@@ -6,6 +6,6 @@ namespace GitWrite.ViewModels
    {
       public CommitViewModel CommitViewModel => SimpleIoc.Default.GetInstance<CommitViewModel>();
 
-      public InteractiveRebaseViewModel InteractiveRebaseViewMdoel => SimpleIoc.Default.GetInstance<InteractiveRebaseViewModel>();
+      public InteractiveRebaseViewModel InteractiveRebaseViewModel => SimpleIoc.Default.GetInstance<InteractiveRebaseViewModel>();
    }
 }
