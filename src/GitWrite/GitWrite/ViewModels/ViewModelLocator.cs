8d8eabb34ab4559f1e75236d2261e9b9e3c42a08
Alex Novak <alexwnovak@gmail.com>
2017-04-23 15:14:12 -0400

    Formatting

--- src/GitWrite/GitWrite/ViewModels/ViewModelLocator.cs
+++ src/GitWrite/GitWrite/ViewModels/ViewModelLocator.cs
@@ -5,7 +5,6 @@ namespace GitWrite.ViewModels
    public class ViewModelLocator
    {
       public CommitViewModel CommitViewModel => SimpleIoc.Default.GetInstance<CommitViewModel>();
-
       public RebaseViewModel RebaseViewModel => SimpleIoc.Default.GetInstance<RebaseViewModel>();
    }
 }
