81fe702e9397fb464ee506711d0349f724a08199
Alex Novak <alexwnovak@gmail.com>
2017-04-23 16:29:14 -0400

    -StoryboardHelper and interface (unused)

--- src/GitWrite/GitWrite/App.xaml.cs
+++ src/GitWrite/GitWrite/App.xaml.cs
@@ -110,7 +110,6 @@ namespace GitWrite
          SimpleIoc.Default.Register<IApplicationSettings, ApplicationSettings>();
          SimpleIoc.Default.Register<ICommitFileReader, CommitFileReader>();
          SimpleIoc.Default.Register<IRebaseFileWriter, RebaseFileWriter>();
-         SimpleIoc.Default.Register<IStoryboardHelper, StoryboardHelper>();
          SimpleIoc.Default.Register<IFileAdapter, FileAdapter>();
          SimpleIoc.Default.Register<IAppService, AppService>();
          SimpleIoc.Default.Register<IClipboardService, ClipboardService>();
