2ad825319f8212e0fb7784490b3e4c5a729f078f
Alex Novak <alexwnovak@gmail.com>
2017-04-23 16:17:13 -0400

    +IRebaseFileWriter blank interface and concrete
    This will take care of the file I/O so the document itself doesn't have to worry about it. Reading/loading is already split off from it, so it doesn't seem like this should be different.

---
+++ src/GitWrite/GitWrite/RebaseFileWriter.cs
@@ -0,0 +1,10 @@
+namespace GitWrite
+{
+   public class RebaseFileWriter : IRebaseFileWriter
+   {
+      public void Save( RebaseDocument rebaseDocument )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
