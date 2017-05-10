65e9c6a1245ff0de40f8df7ec3b88d583c615a9c
Alex Novak <alexwnovak@gmail.com>
2017-05-09 20:10:40 -0400

    +"With" methods to build out the object state

--- src/GitWrite/GitWrite.UnitTests/Internal/Builders/CommitViewModelBuilder.cs
+++ src/GitWrite/GitWrite.UnitTests/Internal/Builders/CommitViewModelBuilder.cs
@@ -12,6 +12,36 @@ namespace GitWrite.UnitTests.Internal.Builders
       private ICommitDocument _commitDocument = Mock.Of<ICommitDocument>();
       private IGitService _gitService = Mock.Of<IGitService>();
 
+      public CommitViewModelBuilder WithViewService( IViewService viewService )
+      {
+         _viewService = viewService;
+         return this;
+      }
+
+      public CommitViewModelBuilder WithAppService( IAppService appService )
+      {
+         _appService = appService;
+         return this;
+      }
+
+      public CommitViewModelBuilder WithClipboardService( IClipboardService clipboardService )
+      {
+         _clipboardService = clipboardService;
+         return this;
+      }
+
+      public CommitViewModelBuilder WithCommitDocument( ICommitDocument commitDocument )
+      {
+         _commitDocument = commitDocument;
+         return this;
+      }
+
+      public CommitViewModelBuilder WithGitService( IGitService gitService )
+      {
+         _gitService = gitService;
+         return this;
+      }
+
       public CommitViewModel Build()
          => new CommitViewModel( _viewService, _appService, _clipboardService, _commitDocument, _gitService );
    }
