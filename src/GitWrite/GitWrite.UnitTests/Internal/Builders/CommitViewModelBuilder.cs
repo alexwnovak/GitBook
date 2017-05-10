df0d3594d9c24a08fd4d3c9797aefc885c1d3a39
Alex Novak <alexwnovak@gmail.com>
2017-05-09 20:04:04 -0400

    +Build with default mock dependencies

--- src/GitWrite/GitWrite.UnitTests/Internal/Builders/CommitViewModelBuilder.cs
+++ src/GitWrite/GitWrite.UnitTests/Internal/Builders/CommitViewModelBuilder.cs
@@ -1,6 +1,18 @@
-﻿namespace GitWrite.UnitTests.Internal.Builders
+﻿using Moq;
+using GitWrite.Services;
+using GitWrite.ViewModels;
+
+namespace GitWrite.UnitTests.Internal.Builders
 {
    internal class CommitViewModelBuilder
    {
+      private IViewService _viewService = Mock.Of<IViewService>();
+      private IAppService _appService = Mock.Of<IAppService>();
+      private IClipboardService _clipboardService = Mock.Of<IClipboardService>();
+      private ICommitDocument _commitDocument = Mock.Of<ICommitDocument>();
+      private IGitService _gitService = Mock.Of<IGitService>();
+
+      public CommitViewModel Build()
+         => new CommitViewModel( _viewService, _appService, _clipboardService, _commitDocument, _gitService );
    }
 }
