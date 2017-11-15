850caf150e9f4ff09e5da020215725153c471e5f
Alex Novak <alexwnovak@gmail.com>
2017-11-14 21:14:08 -0500

    WIP

--- src/GitWrite/GitWrite/Views/Converters/TextLengthInversionConverter.cs
+++ src/GitWrite/GitWrite/Views/Converters/TextLengthInversionConverter.cs
@@ -7,9 +7,63 @@ using GitWrite.Services;
 
 namespace GitWrite.Views.Converters
 {
+   public class ValueConverter<TFrom, TTo> : MarkupExtension, IValueConverter
+   {
+      private static readonly ValueConverter<TFrom, TTo> _sharedInstance = new ValueConverter<TFrom, TTo>();
+
+      public bool IsShared
+      {
+         get;
+         set;
+      }
+
+      protected ValueConverter()
+      {
+      }
+
+      public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+      {
+         if ( !(value is TFrom) )
+         {
+            return null;
+         }
+
+         return ConvertCore( (TFrom) value );
+      }
+
+      protected virtual TTo ConvertCore( TFrom value )
+      {
+         return default( TTo );
+      }
+
+      public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+
+      public override object ProvideValue( IServiceProvider serviceProvider )
+         => IsShared ? _sharedInstance : this;
+   }
+
+   public class NewConverter : ValueConverter<int, string>
+   {
+      protected override string ConvertCore( int value )
+      {
+         return "69";
+      }
+   }
+
    public class TextLengthInversionConverter : MarkupExtension, IValueConverter
    {
+      private static readonly TextLengthInversionConverter _sharedInstance = new TextLengthInversionConverter();
+
       private readonly int _maxLength;
+
+      public bool IsShared
+      {
+         get;
+         set;
+      }
 
       public TextLengthInversionConverter()
       {
@@ -22,7 +76,10 @@ namespace GitWrite.Views.Converters
          _maxLength = appSettings.MaxCommitLength;
       }
 
-      public override object ProvideValue( IServiceProvider serviceProvider ) => this;
+      public override object ProvideValue( IServiceProvider serviceProvider )
+      {
+         return IsShared ? _sharedInstance : this;
+      }
 
       public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
       {
