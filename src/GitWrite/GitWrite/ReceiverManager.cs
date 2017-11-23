1922cff758c78ce775f910aba3f1ff9afd74b71b
Alex Novak <alexwnovak@gmail.com>
2017-11-23 15:31:05 -0500

    ReceiverManager now discovers all receivers and keeps track of them
    By creating them, they do their hook-up through the parent class to the messenger.

--- src/GitWrite/GitWrite/ReceiverManager.cs
+++ src/GitWrite/GitWrite/ReceiverManager.cs
@@ -1,6 +1,28 @@
-﻿namespace GitWrite
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using GitWrite.Receivers;
+
+namespace GitWrite
 {
    public class ReceiverManager
    {
+      private readonly List<object> _receivers = new List<object>();
+
+      public void Initialize()
+      {
+         var thisAssembly = Assembly.GetExecutingAssembly();
+
+         var allReceivers = thisAssembly.GetTypes().Where( t => t.BaseType != null
+            && t.BaseType.IsGenericType
+            && t.BaseType?.GetGenericTypeDefinition() == typeof( MessageReceiver<> ) );
+
+         foreach ( var receiver in allReceivers )
+         {
+            var receiverInstance = Activator.CreateInstance(receiver);
+            _receivers.Add( receiverInstance );
+         }
+      }
    }
 }
