37694d591e1b1eaf4ac2b8c44eb3aaae01e204bd
Alex Novak <alexwnovak@gmail.com>
2018-03-13 22:09:32 -0400

    Settings button now goes to the receiver

---
+++ src/GitWrite/GitWrite/Receivers/ShowSettingsReceiver.cs
@@ -0,0 +1,11 @@
+﻿using GitWrite.Messages;
+
+namespace GitWrite.Receivers
+{
+   public class ShowSettingsReceiver : MessageReceiver<ShowSettingsMessage>
+   {
+      protected override void OnReceive( ShowSettingsMessage message )
+      {
+      }
+   }
+}
