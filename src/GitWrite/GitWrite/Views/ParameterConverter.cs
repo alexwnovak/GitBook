7d74f8864c387337a695f4edf453ad267baa2776
Alex Novak <alexwnovak@gmail.com>
2018-03-14 22:29:03 -0400

    +Converter to turn the multi-binding into an ObservableCollection
    This is what the EventToMessage needs to ingest, and a multi-binding needs a converter. This makes the API a little clunky, in that you have to use this converter, but it still accomplishes what we want.

---
+++ src/GitWrite/GitWrite/Views/ParameterConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace GitWrite.Views
+{
+   public class ParameterConverter : MarkupExtension, IMultiValueConverter
+   {
+      private static readonly ParameterConverter _instance = new ParameterConverter();
+
+      public override object ProvideValue( IServiceProvider serviceProvider ) => _instance;
+
+      public object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+      {
+         return new ObservableCollection<object>( values );
+      }
+
+      public object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
