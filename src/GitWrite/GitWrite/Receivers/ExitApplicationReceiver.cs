08e790578813ce756758a95ff18b17c92a9d7885
Alex Novak <alexwnovak@gmail.com>
2018-03-17 09:04:31 -0400

    +Receiver for exiting the application

---
+++ src/GitWrite/GitWrite/Receivers/ExitApplicationReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using GitWrite.Messages;
+
+namespace GitWrite.Receivers
+{
+   public class ExitApplicationReceiver : MessageReceiver<ExitApplicationMessage>
+   {
+      private readonly Window _window;
+
+      public ExitApplicationReceiver( Window window )
+      {
+         _window = window;
+      }
+
+      protected override void OnReceive( ExitApplicationMessage message )
+      {
+         _window.Close();
+      }
+   }
+}
