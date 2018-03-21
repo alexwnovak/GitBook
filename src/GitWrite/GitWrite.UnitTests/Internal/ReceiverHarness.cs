239887a30c2c7e5ce397b24e3a2b741c17b4b106
Alex Novak <alexwnovak@gmail.com>
2018-03-20 23:22:43 -0400

    +ReceiverHarness test class to smooth out testing receivers
    This guy will automatically create and register a new messenger to a receiver instance. From there you can pipe a message to it so the receiver's OnReceive method is called (the real system under test). The harness can also "Verify" that messages were sent (similar to a Moq Verify). This is useful when the receiver-under-test goes on to send other messages as part of its implementation.

---
+++ src/GitWrite/GitWrite.UnitTests/Internal/ReceiverHarness.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GalaSoft.MvvmLight.Messaging;
+using GitWrite.Receivers;
+using FluentAssertions.Execution;
+
+namespace GitWrite.UnitTests.Internal
+{
+   internal class ReceiverHarness<TReceiver> where TReceiver : IMessageReceiver
+   {
+      private readonly InstrumentedMessenger _messenger = new InstrumentedMessenger();
+
+      public ReceiverHarness( TReceiver receiver )
+      {
+         receiver.SetMessenger( _messenger );
+         receiver.Register();
+      }
+
+      public void Send<TMessage>( TMessage message ) where TMessage : MessageBase
+      {
+         _messenger.Send( message );
+      }
+
+      public void VerifySend<TMessage>( Func<TMessage, bool> predicate ) where TMessage : MessageBase
+      {
+         foreach ( var thing in _messenger.SentMessages )
+         {
+            if ( predicate( (TMessage) thing ) )
+            {
+               return;
+            }
+         }
+
+         throw new AssertionFailedException( $"Receiver harness did not send message of type {typeof( TMessage ).Name}" );
+      }
+   }
+}
