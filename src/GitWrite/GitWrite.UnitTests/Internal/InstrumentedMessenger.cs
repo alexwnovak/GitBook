e876b5e088198891a9519370a64527aa8f87dbc2
Alex Novak <alexwnovak@gmail.com>
2018-03-20 23:15:23 -0400

    +InstrumentedMessenger class for supporting messenger-based tests
    This acts like a messenger, but also keeps a list of all the message it sends so it can report later when asked what it did.

---
+++ src/GitWrite/GitWrite.UnitTests/Internal/InstrumentedMessenger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace GitWrite.UnitTests.Internal
+{
+   internal class InstrumentedMessenger : Messenger
+   {
+      private readonly List<MessageBase> _sentMessages = new List<MessageBase>();
+      public IEnumerable<MessageBase> SentMessages => _sentMessages;
+
+      public new void Send<TMessage>( TMessage message ) where TMessage : MessageBase
+      {
+         _sentMessages.Add( message );
+         base.Send( message );
+      }
+   }
+}
