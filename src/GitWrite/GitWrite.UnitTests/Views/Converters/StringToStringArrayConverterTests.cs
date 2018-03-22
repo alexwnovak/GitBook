d56026e04193255e30a5c61b3308c02f5625d84f
Alex Novak <alexwnovak@gmail.com>
2018-03-21 21:27:01 -0400

    First pass at a string-to-string-array converter
    We'll strap this to the "body" binding. GitModel gives us an array of strings as the body, instead of one big string, and we're binding to a text box, which is one big string. So this converter will help us go between both without logic in the view model.
    
    This first part translates from a string[] -> string (pushing into the text box from the view model). The next part will go the other direction--pushing into the model as the UI changes.

---
+++ src/GitWrite/GitWrite.UnitTests/Views/Converters/StringToStringArrayConverterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using FluentAssertions;
+using GitWrite.Views.Converters;
+using Xunit;
+
+namespace GitWrite.UnitTests.Views.Converters
+{
+   public class StringToStringArrayConverterTests
+   {
+      [Fact]
+      public void Convert_ValueIsNull_ReturnsEmptyString()
+      {
+         var converter = new StringToStringArrayConverter();
+
+         var convertedValue = converter.Convert( null, null, null, null );
+
+         convertedValue.Should().Be( string.Empty );
+      }
+
+      [Fact]
+      public void Convert_ValueIsNotStringArray_ReturnsEmptyString()
+      {
+         var converter = new StringToStringArrayConverter();
+
+         var convertedValue = converter.Convert( 5, null, null, null );
+
+         convertedValue.Should().Be( string.Empty );
+      }
+
+      [Fact]
+      public void Convert_ValueIsEmptyStringArray_ReturnsEmptyString()
+      {
+         var converter = new StringToStringArrayConverter();
+
+         var convertedValue = converter.Convert( new string[0], null, null, null );
+
+         convertedValue.Should().Be( string.Empty );
+      }
+
+      [Fact]
+      public void Convert_ValueHasOneLine_ReturnsLine()
+      {
+         var lines = new[]
+         {
+            "First line"
+         };
+
+         var converter = new StringToStringArrayConverter();
+
+         var convertedValue = converter.Convert( lines, null, null, null );
+
+         convertedValue.Should().Be( "First line" );
+      }
+
+      [Fact]
+      public void Convert_ValueHasMultipleLines_ReturnsOneLineWithLineBreaks()
+      {
+         var lines = new[]
+         {
+            "First line",
+            "Second line"
+         };
+
+         var converter = new StringToStringArrayConverter();
+
+         var convertedValue = converter.Convert( lines, null, null, null );
+
+         convertedValue.Should().Be( $"First line{Environment.NewLine}Second line" );
+      }
+   }
+}
---
+++ src/GitWrite/GitWrite/Views/Converters/StringToStringArrayConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace GitWrite.Views.Converters
+{
+   public class StringToStringArrayConverter : MarkupExtension, IValueConverter
+   {
+      public override object ProvideValue( IServiceProvider serviceProvider ) => this;
+
+      public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+      {
+         if ( value == null )
+         {
+            return string.Empty;
+         }
+
+         if ( !( value is string[] stringArray ) )
+         {
+            return string.Empty;
+         }
+
+         if ( stringArray.Length == 0 )
+         {
+            return string.Empty;
+         }
+
+         return string.Join( Environment.NewLine, stringArray );
+      }
+
+      public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
