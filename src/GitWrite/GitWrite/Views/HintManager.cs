35e804b84425e59150bdf38595907c4c90b3695a
Alex Novak <alexwnovak@gmail.com>
2018-03-22 21:34:55 -0400

    +HintManager and HintText attached property for text boxes
    We'll use this as bolt-on info for styles to display extra stuff.

---
+++ src/GitWrite/GitWrite/Views/HintManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitWrite.Views
+{
+   public static class HintManager
+   {
+      private static readonly Dictionary<TextBox, string> _hintTable = new Dictionary<TextBox, string>();
+
+      public static readonly DependencyProperty HintTextProperty = DependencyProperty.RegisterAttached(
+         "HintText",
+         typeof( string ),
+         typeof( HintManager ),
+         new PropertyMetadata( "" ) );
+
+      public static string GetHintText( TextBox textBox ) => (string) textBox.GetValue( HintTextProperty );
+      public static void SetHintText( TextBox textBox, string hintText ) => textBox.SetValue( HintTextProperty, hintText );
+   }
+}
