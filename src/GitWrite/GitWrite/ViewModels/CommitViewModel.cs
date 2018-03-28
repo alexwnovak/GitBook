94738b5f63e7ce6b2c2a61c047133b2a03ae7efa
Alex Novak <alexwnovak@gmail.com>
2018-03-27 21:29:57 -0400

    Mass clean

--- src/GitWrite/GitWrite/ViewModels/CommitViewModel.cs
+++ src/GitWrite/GitWrite/ViewModels/CommitViewModel.cs
@@ -1,10 +1,6 @@
-using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using GalaSoft.MvvmLight.Messaging;
 using GitModel;
-using GitWrite.Messages;
-using Resx = GitWrite.Properties.Resources;
 
 namespace GitWrite.ViewModels
 {
@@ -17,16 +13,8 @@ namespace GitWrite.ViewModels
       public RelayCommand DiscardCommand { get; }
       public RelayCommand SettingsCommand { get; }
 
-      public bool IsDirty
-      {
-         get;
-         set;
-      }
-
       public CommitViewModel( string commitFilePath,
-         CommitDocument commitDocument,
-         IMessenger messenger )
-         : base( messenger )
+         CommitDocument commitDocument )
       {
          CommitFilePath = commitFilePath;
          CommitDocument = commitDocument;
@@ -34,8 +22,6 @@ namespace GitWrite.ViewModels
          AcceptCommand = new RelayCommand( OnAcceptCommand );
          DiscardCommand = new RelayCommand( OnDiscardCommand );
          SettingsCommand = new RelayCommand( OnSettingsCommand );
-
-         IsDirty = false;
       }
 
       private void OnAcceptCommand()
@@ -48,124 +34,6 @@ namespace GitWrite.ViewModels
 
       private void OnSettingsCommand()
       {
-      }
-
-      //public void ViewLoaded()
-      //{
-      //   if ( !string.IsNullOrEmpty( ExtraCommitText ) )
-      //   {
-      //      ExpandUI();
-      //   }
-      //}
-
-      //private async void OnSaveCommand()
-      //{
-      //   ExitReason = ExitReason.Save;
-
-      //   bool shouldContinue = await OnSaveAsync();
-
-      //   if ( !shouldContinue )
-      //   {
-      //      return;
-      //   }
-
-      //   MessengerInstance.Send( new ShutdownRequestedMessage() );
-      //}
-
-      //private async void OnAbortCommand()
-      //{
-      //   if ( IsExiting )
-      //   {
-      //      return;
-      //   }
-
-      //   ExitReason = ExitReason.Discard;
-      //   ExitReason exitReason = ExitReason.Discard;
-
-      //   if ( IsDirty )
-      //   {
-      //      var confirmationResult = ViewService.ConfirmExit();
-
-      //      if ( confirmationResult == ExitReason.Cancel )
-      //      {
-      //         return;
-      //      }
-
-      //      if ( confirmationResult == ExitReason.Save )
-      //      {
-      //         ExitReason = ExitReason.Save;
-
-      //         bool shouldReallyExit = await OnSaveAsync();
-
-      //         if ( !shouldReallyExit )
-      //         {
-      //            return;
-      //         }
-      //      }
-      //      else
-      //      {
-      //         await OnDiscardAsync();
-      //         IsExiting = true;
-      //      }
-      //   }
-      //   else if ( exitReason == ExitReason.Discard )
-      //   {
-      //      await OnDiscardAsync();
-      //   }
-
-      //   IsExiting = true;
-      //   MessengerInstance.Send( new ShutdownRequestedMessage() );
-      //}
-
-      protected async Task OnExitRequestedAsync( ExitReason exitReason )
-      {
-         var message = new ExitRequestedMessage( exitReason );
-
-         MessengerInstance.Send( message );
-
-         await message.Task;
-      }
-
-      //protected async Task<bool> OnSaveAsync()
-      //{
-      //   if ( string.IsNullOrWhiteSpace( ShortMessage ) || IsExiting )
-      //   {
-      //      MessengerInstance.Send( new ShakeRequestedMessage() );
-      //      return false;
-      //   }
-
-      //   IsExiting = true;
-
-      //   CollapseUI();
-
-      //   await OnExitRequestedAsync( ExitReason.Save );
-
-      //   _commitDocument.Subject = ShortMessage;
-
-      //   if ( string.IsNullOrEmpty( ExtraCommitText ) )
-      //   {
-      //      _commitDocument.Body = null;
-      //   }
-      //   else
-      //   {
-      //      _commitDocument.Body = ExtraCommitText.Split( new[] { Environment.NewLine }, StringSplitOptions.None );
-      //   }
-
-      //   MessengerInstance.Send( new WriteCommitDocumentMessage( _commitFilePath, _commitDocument ) );
-
-      //   return true;
-      //}
-
-      protected async Task<bool> OnDiscardAsync()
-      {
-         await OnExitRequestedAsync( ExitReason.Discard );
-
-         CommitDocument.Subject = null;
-         CommitDocument.Body = new string[0];
-
-         MessengerInstance.Send( new WriteCommitDocumentMessage( CommitFilePath, CommitDocument ) );
-
-         return true;
       }
    }
 }
