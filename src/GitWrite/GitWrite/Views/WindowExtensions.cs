342bf48ca589cf5b5a8ebe23dd8a2dc92af499f6
Alex Novak <alexwnovak@gmail.com>
2018-03-28 21:25:16 -0400

    +Extension method for playing a storyboard from a window

---
+++ src/GitWrite/GitWrite/Views/WindowExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace GitWrite.Views
+{
+   public static class WindowExtensions
+   {
+      public static void PlayStoryboard( this Window window, string storyboardName )
+      {
+         var storyboard = (Storyboard) window.Resources["SubjectHint"];
+         storyboard.Begin();
+      }
+   }
+}
