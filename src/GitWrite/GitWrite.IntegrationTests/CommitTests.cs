a1113f66fdf948f9f1f63d85c33fe04086db51e5
Alex Novak <alexwnovak@gmail.com>
2018-04-02 22:52:16 -0400

    +Integration test to show that commit details can be edited then saved

--- src/GitWrite/GitWrite.IntegrationTests/CommitTests.cs
+++ src/GitWrite/GitWrite.IntegrationTests/CommitTests.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using FluentAssertions;
+using GitModel;
+using GitWrite.IntegrationTests.Infrastructure;
 
 namespace GitWrite.IntegrationTests
 {
-   class CommitTests
+   public class CommitTests
    {
+      [Fact]
+      public void CommitDetailsAreEditedThenSaved()
+      {
+         var commitScenario = CommitScenarioBuilder.Create().Build();
+
+         commitScenario.SetSubject( "Something else" );
+         commitScenario.SetBody( new[] { "Body line 1", "Body line 2" } );
+         commitScenario.AcceptChanges();
+
+         var actualCommit = new CommitFileReader().FromFile( commitScenario.FilePath );
+         actualCommit.Subject.Should().Be( "Something else" );
+         actualCommit.Body[0].Should().Be( "Body line 1" );
+         actualCommit.Body[1].Should().Be( "Body line 2" );
+      }
    }
 }
