cb315c45cc21515e4f1b6e3a9f55165efabbbbfe
Alex Novak <alexwnovak@gmail.com>
2018-04-05 22:38:09 -0400

    Now tries to convert the setting into the target property type

--- src/GitWrite.UnitTests/Views/SettingsExtensionTests.cs
+++ src/GitWrite.UnitTests/Views/SettingsExtensionTests.cs
@@ -1,6 +1,51 @@
-﻿namespace GitWrite.UnitTests.Views
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Media;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using GitWrite.Services;
+using GitWrite.Views;
+
+namespace GitWrite.UnitTests.Views
 {
    public class SettingsExtensionTests
    {
+      [Fact]
+      public void ProvideValue_RetrievesValidColorString_IsConvertedToBrush()
+      {
+         var applicationSettingsMock = new Mock<IApplicationSettings>();
+         applicationSettingsMock.Setup( @as => @as.GetSetting( "MainColor" ) ).Returns( "#FF0000" );
+
+         var provideValueTargetMock = new Mock<IProvideValueTarget>();
+         provideValueTargetMock.SetupGet( pvt => pvt.TargetProperty ).Returns( Panel.BackgroundProperty );
+         var serviceProviderMock = new Mock<IServiceProvider>();
+         serviceProviderMock.Setup( sp => sp.GetService( typeof( IProvideValueTarget ) ) ).Returns( provideValueTargetMock.Object );
+
+         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, "MainColor" );
+         var value = (SolidColorBrush) settingsExtension.ProvideValue( serviceProviderMock.Object );
+
+         value.Color.Should().Be( Colors.Red );
+      }
+
+      [Fact]
+      public void ProvideValue_RetrievesValidInteger_ReturnsInteger()
+      {
+         var applicationSettingsMock = new Mock<IApplicationSettings>();
+         applicationSettingsMock.Setup( @as => @as.GetSetting( "MaxLength" ) ).Returns( 50 );
+
+         var provideValueTargetMock = new Mock<IProvideValueTarget>();
+         provideValueTargetMock.SetupGet( pvt => pvt.TargetProperty ).Returns( TextBox.MaxLengthProperty );
+         var serviceProviderMock = new Mock<IServiceProvider>();
+         serviceProviderMock.Setup( sp => sp.GetService( typeof( IProvideValueTarget ) ) ).Returns( provideValueTargetMock.Object );
+
+         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, "MaxLength" );
+         var value = (int) settingsExtension.ProvideValue( serviceProviderMock.Object );
+
+         value.Should().Be( 50 );
+      }
+
    }
 }
--- src/GitWrite/Views/SettingsExtension.cs
+++ src/GitWrite/Views/SettingsExtension.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Markup;
 using GalaSoft.MvvmLight.Ioc;
 using GitWrite.Services;
@@ -7,15 +9,34 @@ namespace GitWrite.Views
 {
    public class SettingsExtension : MarkupExtension
    {
-      private readonly IApplicationSettings _applicationSettings = SimpleIoc.Default.GetInstance<IApplicationSettings>();
+      private readonly IApplicationSettings _applicationSettings;
       private readonly string _name;
 
       public SettingsExtension( string name )
+         : this( SimpleIoc.Default.GetInstance<IApplicationSettings>() , name )
       {
+      }
+
+      public SettingsExtension( IApplicationSettings applicationSettings, string name )
+      {
+         _applicationSettings = applicationSettings;
          _name = name;
       }
 
-      public override object ProvideValue( IServiceProvider serviceProvider ) =>
-         _applicationSettings.GetSetting( _name );
+      public override object ProvideValue( IServiceProvider serviceProvider )
+      {
+         var valueTarget = (IProvideValueTarget) serviceProvider.GetService( typeof( IProvideValueTarget ) );
+         var targetProperty = (DependencyProperty) valueTarget.TargetProperty;
+
+         object value = _applicationSettings.GetSetting( _name );
+
+         if ( value.GetType() == targetProperty.PropertyType )
+         {
+            return value;
+         }
+
+         var valueConverter = TypeDescriptor.GetConverter( targetProperty.PropertyType );
+         return valueConverter.ConvertFrom( value );
+      }
    }
 }
