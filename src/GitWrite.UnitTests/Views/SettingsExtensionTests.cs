56005ebf2a70c184bde03a8d37266403010bc902
Alex Novak <alexwnovak@gmail.com>
2018-04-07 10:41:00 -0400

    SettingsExtension can now refresh a setting via message / name
    When the message is sent with the name as the token, the SettingsExtension will re-read the setting from the interface and re-push it into the associated dependency object and property it's linked to.

--- src/GitWrite.UnitTests/Views/SettingsExtensionTests.cs
+++ src/GitWrite.UnitTests/Views/SettingsExtensionTests.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Markup;
 using System.Windows.Media;
 using Xunit;
 using Moq;
 using FluentAssertions;
+using GalaSoft.MvvmLight.Messaging;
 using GitWrite.Services;
 using GitWrite.Views;
 
@@ -24,7 +25,7 @@ namespace GitWrite.UnitTests.Views
          var serviceProviderMock = new Mock<IServiceProvider>();
          serviceProviderMock.Setup( sp => sp.GetService( typeof( IProvideValueTarget ) ) ).Returns( provideValueTargetMock.Object );
 
-         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, "MainColor" );
+         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, Mock.Of<IMessenger>(), "MainColor" );
          var value = (SolidColorBrush) settingsExtension.ProvideValue( serviceProviderMock.Object );
 
          value.Color.Should().Be( Colors.Red );
@@ -41,11 +42,36 @@ namespace GitWrite.UnitTests.Views
          var serviceProviderMock = new Mock<IServiceProvider>();
          serviceProviderMock.Setup( sp => sp.GetService( typeof( IProvideValueTarget ) ) ).Returns( provideValueTargetMock.Object );
 
-         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, "MaxLength" );
+         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, Mock.Of<IMessenger>(), "MaxLength" );
          var value = (int) settingsExtension.ProvideValue( serviceProviderMock.Object );
 
          value.Should().Be( 50 );
       }
 
+      [Fact]
+      public void RefreshSettingsMessage_RefreshingColor_SetsTheColor()
+      {
+         var applicationSettingsMock = new Mock<IApplicationSettings>();
+         applicationSettingsMock.SetupSequence( @as => @as.GetSetting( "MainColor" ) )
+            .Returns( "#0000FF" )
+            .Returns( "#FF0000" );
+
+         var messenger = new Messenger();
+         var associatedObject = new DependencyObject();
+
+         var provideValueTargetMock = new Mock<IProvideValueTarget>();
+         provideValueTargetMock.SetupGet( pvt => pvt.TargetProperty ).Returns( Panel.BackgroundProperty );
+         provideValueTargetMock.SetupGet( pvt => pvt.TargetObject ).Returns( associatedObject );
+         var serviceProviderMock = new Mock<IServiceProvider>();
+         serviceProviderMock.Setup( sp => sp.GetService( typeof( IProvideValueTarget ) ) ).Returns( provideValueTargetMock.Object );
+
+         var settingsExtension = new SettingsExtension( applicationSettingsMock.Object, messenger, "MainColor" );
+         settingsExtension.ProvideValue( serviceProviderMock.Object );
+
+         messenger.Send( new RefreshSettingsMessage(), "MainColor" );
+
+         var value = (SolidColorBrush) associatedObject.GetValue( Panel.BackgroundProperty );
+         value.Color.Should().Be( Colors.Red );
+      }
    }
 }
--- src/GitWrite/Views/SettingsExtension.cs
+++ src/GitWrite/Views/SettingsExtension.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Markup;
 using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
 using GitWrite.Services;
 
 namespace GitWrite.Views
@@ -12,31 +13,48 @@ namespace GitWrite.Views
       private readonly IApplicationSettings _applicationSettings;
       private readonly string _name;
 
+      private DependencyObject _targetObject;
+      private DependencyProperty _targetProperty;
+
       public SettingsExtension( string name )
-         : this( SimpleIoc.Default.GetInstance<IApplicationSettings>() , name )
+         : this( SimpleIoc.Default.GetInstance<IApplicationSettings>(), Messenger.Default, name )
       {
       }
 
-      public SettingsExtension( IApplicationSettings applicationSettings, string name )
+      public SettingsExtension( IApplicationSettings applicationSettings, IMessenger messenger, string name )
       {
          _applicationSettings = applicationSettings;
          _name = name;
+
+         messenger.Register<RefreshSettingsMessage>( this, name, _ => OnRefreshSettings(), true );
+      }
+
+      private object GetSettingValue()
+      {
+         object value = _applicationSettings.GetSetting( _name );
+
+         if ( value.GetType() == _targetProperty.PropertyType )
+         {
+            return value;
+         }
+
+         var valueConverter = TypeDescriptor.GetConverter( _targetProperty.PropertyType );
+         return valueConverter.ConvertFrom( value );
+      }
+
+      private void OnRefreshSettings()
+      {
+         var settingValue = GetSettingValue();
+         _targetObject.SetValue( _targetProperty, settingValue );
       }
 
       public override object ProvideValue( IServiceProvider serviceProvider )
       {
          var valueTarget = (IProvideValueTarget) serviceProvider.GetService( typeof( IProvideValueTarget ) );
-         var targetProperty = (DependencyProperty) valueTarget.TargetProperty;
+         _targetObject = (DependencyObject) valueTarget.TargetObject;
+         _targetProperty = (DependencyProperty) valueTarget.TargetProperty;
 
-         object value = _applicationSettings.GetSetting( _name );
-
-         if ( value.GetType() == targetProperty.PropertyType )
-         {
-            return value;
-         }
-
-         var valueConverter = TypeDescriptor.GetConverter( targetProperty.PropertyType );
-         return valueConverter.ConvertFrom( value );
+         return GetSettingValue();
       }
    }
 }
