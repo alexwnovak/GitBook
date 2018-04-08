32776ebe4868db484004330f79aaac4bb488f97e
Alex Novak <alexwnovak@gmail.com>
2018-04-08 10:21:17 -0400

    Extension now generates an accent based on the UI brightness

--- src/GitWrite.UnitTests/Views/AccentColorExtensionTests.cs
+++ src/GitWrite.UnitTests/Views/AccentColorExtensionTests.cs
@@ -1,6 +1,45 @@
-﻿namespace GitWrite.UnitTests.Views
+﻿using System;
+using System.Windows.Media;
+using FluentAssertions;
+using GitWrite.Services;
+using GitWrite.Views;
+using Moq;
+using Xunit;
+
+namespace GitWrite.UnitTests.Views
 {
    public class AccentColorExtensionTests
    {
+      [Fact]
+      public void ProvideValue_BackgroundIsBright_GeneratesTranslucentDarkColor()
+      {
+         var appSettingsMock = new Mock<IApplicationSettings>();
+         appSettingsMock.Setup( @as => @as.GetSetting( "BackgroundColor" ) ).Returns( "#FFF" );
+
+         var extension = new AccentColorExtension( appSettingsMock.Object )
+         {
+            Opacity = 0.2
+         };
+
+         var brush = (SolidColorBrush) extension.ProvideValue( Mock.Of<IServiceProvider>() );
+
+         brush.Color.Should().BeEquivalentTo( Color.FromArgb( 51, 0, 0, 0 ) );
+      }
+
+      [Fact]
+      public void ProvideValue_BackgroundIsDark_GeneratesTranslucentLightkColor()
+      {
+         var appSettingsMock = new Mock<IApplicationSettings>();
+         appSettingsMock.Setup( @as => @as.GetSetting( "BackgroundColor" ) ).Returns( "#000" );
+
+         var extension = new AccentColorExtension( appSettingsMock.Object )
+         {
+            Opacity = 0.2
+         };
+
+         var brush = (SolidColorBrush) extension.ProvideValue( Mock.Of<IServiceProvider>() );
+
+         brush.Color.Should().BeEquivalentTo( Color.FromArgb( 51, 255, 255, 255 ) );
+      }
    }
 }
