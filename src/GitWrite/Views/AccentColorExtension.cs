33d5461d372a66722f0e5daaeebcdc3f716baeb4
Alex Novak <alexwnovak@gmail.com>
2018-04-08 10:10:39 -0400

    +Blank class for AccentColorExtension and test class
    This will be used to generate accent colors. This will read the background color as the "main color," then decide what kind of mostly-transparent color to use on top of it, depending on the overall brightness of the UI.

---
+++ src/GitWrite/Views/AccentColorExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace GitWrite.Views
+{
+   [MarkupExtensionReturnType( typeof( Brush ) )]
+   public class AccentColorExtension : MarkupExtension
+   {
+      public override object ProvideValue( IServiceProvider serviceProvider )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
