9557312488a6553444297371df1236758673426f
Alex Novak <alexwnovak@gmail.com>
2018-04-08 22:39:27 -0400

    +Blank settings window

---
+++ src/GitWrite/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace GitWrite.Views
+{
+   public partial class SettingsWindow : Window
+   {
+      public SettingsWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
