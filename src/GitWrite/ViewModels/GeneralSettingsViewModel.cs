f71e7ed806a6ad1d117c4766b5e056fe409a7232
Alex Novak <alexwnovak@gmail.com>
2018-04-09 21:53:50 -0400

    +View model for the "General" settings section

---
+++ src/GitWrite/ViewModels/GeneralSettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GalaSoft.MvvmLight;
+using Resx = GitWrite.Properties.Resources;
+
+namespace GitWrite.ViewModels
+{
+   public class GeneralSettingsViewModel : ViewModelBase, ISettingsSectionViewModel
+   {
+      public string Header => Resx.GeneralText;
+   }
+}
