cebaef6d790fb8f96bc6d99380dfa4b633e6305b
Alex Novak <alexwnovak@gmail.com>
2018-04-09 21:59:05 -0400

    +SettingsViewModel
    This guy aggregates sections. This view model represents the entire window, where the sections represent various setting pages.

---
+++ src/GitWrite/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+namespace GitWrite.ViewModels
+{
+   public class SettingsViewModel : ViewModelBase
+   {
+      public ObservableCollection<ISettingsSectionViewModel> Sections { get; }
+
+      public SettingsViewModel( IEnumerable<ISettingsSectionViewModel> settingsSections )
+      {
+         Sections = new ObservableCollection<ISettingsSectionViewModel>( settingsSections );
+      }
+   }
+}
