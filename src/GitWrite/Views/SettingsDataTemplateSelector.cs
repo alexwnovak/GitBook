686bd6d1448610aaf5c4d36c7de9c2e69a9f9da4
Alex Novak <alexwnovak@gmail.com>
2018-04-09 22:00:49 -0400

    +DataTemplateSelector for the setting sections
    This is the thing that knows what template to apply given the view model type that's being displayed. This thing connects the GeneralSettingsViewModel with the right template to actually display its data.

---
+++ src/GitWrite/Views/SettingsDataTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GitWrite.ViewModels;
+
+namespace GitWrite.Views
+{
+   public class SettingsDataTemplateSelector : DataTemplateSelector
+   {
+      public override DataTemplate SelectTemplate( object item, DependencyObject container )
+      {
+         var frameworkElement = (FrameworkElement) container;
+
+         if ( item is GeneralSettingsViewModel )
+         {
+            return frameworkElement.FindResource( "GeneralSettingsDataTemplate" ) as DataTemplate;
+         }
+
+         return base.SelectTemplate( item, container );
+      }
+   }
+}
