e5cbc89a83f6059732e71ffc76005548af94a3ab
Alex Novak <alexwnovak@gmail.com>
2018-04-15 22:50:30 -0400

    Simplifying the Container property

--- src/GitWrite/CommitObjectComposer.cs
+++ src/GitWrite/CommitObjectComposer.cs
@@ -9,25 +9,23 @@ namespace GitWrite
 {
    public class CommitObjectComposer : IObjectComposer
    {
-      private readonly ISimpleIoc _container;
+      public ISimpleIoc Container { get; }
 
       public CommitObjectComposer( ISimpleIoc container )
       {
-         _container = container;
+         Container = container;
       }
-
-      public ISimpleIoc Container => _container;
 
       public void Compose()
       {
-         _container.Register<IApplicationSettings, ApplicationSettings>();
-         _container.Register<ICommitFileReader, CommitFileReader>();
-         _container.Register<ICommitFileWriter, CommitFileWriter>();
-         _container.Register<IRebaseFileWriter, RebaseFileWriter>();
-         _container.Register<IViewService, ViewService>();
-         _container.Register<IRegistryService, RegistryService>();
-         _container.Register<CommitViewModel>();
-         _container.Register<Func<Window>>( () => () => SimpleIoc.Default.GetInstance<Window>() );
+         Container.Register<IApplicationSettings, ApplicationSettings>();
+         Container.Register<ICommitFileReader, CommitFileReader>();
+         Container.Register<ICommitFileWriter, CommitFileWriter>();
+         Container.Register<IRebaseFileWriter, RebaseFileWriter>();
+         Container.Register<IViewService, ViewService>();
+         Container.Register<IRegistryService, RegistryService>();
+         Container.Register<CommitViewModel>();
+         Container.Register<Func<Window>>( () => () => SimpleIoc.Default.GetInstance<Window>() );
       }
    }
 }
