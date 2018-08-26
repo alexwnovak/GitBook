25acf9f45c8e051a9f7c0d7c139711bfa61ffae5
Alex Novak <alexwnovak@gmail.com>
2018-08-26 11:34:44 -0400

    Updating ApplicationSettings tests

--- src/GitWrite.UnitTests/Services/ApplicationSettingsTests.cs
+++ src/GitWrite.UnitTests/Services/ApplicationSettingsTests.cs
@@ -1,20 +1,22 @@
 ﻿using Xunit;
 using Moq;
 using FluentAssertions;
+using AutoFixture.Xunit2;
 using GitWrite.Services;
+using GitWrite.UnitTests.Infrastructure;
 
 namespace GitWrite.UnitTests.Services
 {
    public class ApplicationSettingsTests
    {
-      [Fact]
-      public void GetValue_ReadingValue_ReadsValue()
+      [Theory, AutoMoqData]
+      public void GetValue_ReadingValue_ReadsValue(
+         [Frozen] Mock<IRegistryService> registryServiceMock,
+         ApplicationSettings sut )
       {
-         var registryServiceMock = new Mock<IRegistryService>();
          registryServiceMock.Setup( rs => rs.GetValue( ApplicationSettings.Path, "Name" ) ).Returns( 123 );
 
-         var appSettings = new ApplicationSettings( registryServiceMock.Object );
-         object value = appSettings.GetSetting( "Name" );
+         object value = sut.GetSetting( "Name" );
 
          value.Should().Be( 123 );
       }
