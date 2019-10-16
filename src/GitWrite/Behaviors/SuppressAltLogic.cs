6bf35f73f7743f7cc58099617ce6c1f908af8794
Alex Novak <alexwnovak@gmail.com>
2019-10-15 21:36:18 -0400

    Ditching unused behavior

--- src/GitWrite/Behaviors/SuppressAltLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Input;
-
-namespace GitWrite.Behaviors
-{
-   public class SuppressAltLogic
-   {
-      public bool ShouldSuppress( Key key ) => key == Key.LeftAlt || key == Key.RightAlt;
-   }
-}
