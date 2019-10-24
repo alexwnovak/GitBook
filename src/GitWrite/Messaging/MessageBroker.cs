2c9b5d78b82dbd080ae949be499be044ea85eaac
Alex Novak <alexwnovak@gmail.com>
2019-10-24 11:52:29 -0400

    +MessageBroker
    This class discovers message receivers, creates them, and hooks them up to the messenger. This is a convention, where any receiver author gets their object worked up for free.

---
+++ src/GitWrite/Messaging/MessageBroker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace GitWrite.Messaging
+{
+   public class MessageBroker
+   {
+      public void Initialize()
+      {
+         var exportedTypes = Assembly.GetExecutingAssembly().GetExportedTypes();
+
+         foreach ( var exportedType in exportedTypes )
+         {
+            bool isReceiver = exportedType.BaseType != null &&
+                              exportedType.BaseType.IsGenericType &&
+                              exportedType.BaseType.GetGenericTypeDefinition() == typeof( MessageReceiver<> );
+
+            if ( isReceiver )
+            {
+               RegisterReceiver( exportedType );
+            }
+         }
+      }
+
+      private void RegisterReceiver( Type receiverType )
+      {
+         var receiverInstance = (IMessageReceiver) Activator.CreateInstance( receiverType );
+         var messageType = receiverType.BaseType.GetGenericArguments().First();
+
+         var registerMethod = typeof( Messenger ).GetMethods().First( m => m.Name == "Register" );
+         var closedRegisterMethod = registerMethod.MakeGenericMethod( messageType );
+
+         var parameters = new object[]
+         {
+            receiverInstance,
+            new Action<MessageBase>( receiverInstance.Receive ),
+            true
+         };
+
+         closedRegisterMethod.Invoke( Messenger.Default, parameters );
+      }
+   }
+}
