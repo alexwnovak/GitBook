4df84e0e201d82e89ff75e4a4457e1e0ed81e694
Alex Novak <alexwnovak@gmail.com>
2019-10-29 20:19:26 -0400

    +TestPlatformProvider
    This simulates what the XamlPlatformProvider does, and how it invokes the lifecycle methods.

---
+++ src/GitWrite.Tests/Internal/TestPlatformProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Action = System.Action;
+
+namespace GitWrite.Tests.Internal
+{
+   public class TestPlatformProvider : IPlatformProvider
+   {
+      public bool WasClosed { get; private set; }
+
+      public bool InDesignMode => throw new NotImplementedException();
+      public bool PropertyChangeNotificationsOnUIThread => throw new NotImplementedException();
+      public void BeginOnUIThread( System.Action action ) => throw new NotImplementedException();
+      public void ExecuteOnFirstLoad( object view, Action<object> handler ) => throw new NotImplementedException();
+      public void ExecuteOnLayoutUpdated( object view, Action<object> handler ) => throw new NotImplementedException();
+      public object GetFirstNonGeneratedView( object view ) => throw new NotImplementedException();
+
+      public Func<CancellationToken, Task> GetViewCloseAction( object viewModel, ICollection<object> views, bool? dialogResult )
+      {
+         return async ct =>
+         {
+            if ( viewModel is IGuardClose guardClose &&
+                 viewModel is IDeactivate deactivate )
+            {
+               bool canClose = await guardClose.CanCloseAsync( ct );
+
+               if ( canClose )
+               {
+                  await deactivate.DeactivateAsync( true, ct );
+                  WasClosed = true;
+               }
+            }
+         };
+      }
+
+      public void OnUIThread( Action action ) => action();
+      public Task OnUIThreadAsync( Func<Task> action ) => action();
+   }
+}
