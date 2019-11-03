265eaa1c082e41d67678f138fb9288a62a629c29
Alex Novak <alexwnovak@gmail.com>
2019-11-03 13:59:26 -0500

    +Temporary folder class
    This manages a temp space and the ability to easily create temp files within it, cleaning itself up under a Disose method.

---
+++ src/GitWrite.Tests/Internal/TemporaryFolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace GitWrite.Tests.Internal
+{
+   internal class TemporaryFolder : IDisposable
+   {
+      public string FolderPath { get; }
+
+      public TemporaryFolder()
+      {
+         FolderPath = Path.Combine( Path.GetTempPath(), $"GitWriteTests_{Guid.NewGuid()}" );
+         Directory.CreateDirectory( FolderPath );
+      }
+
+      public string CreateFile( string contents )
+      {
+         string fullPath = Path.Combine( FolderPath, Guid.NewGuid().ToString() );
+         File.WriteAllText( fullPath, contents );
+         return fullPath;
+      }
+
+      public void Dispose()
+      {
+         Directory.Delete( FolderPath, true );
+      }
+   }
+}
