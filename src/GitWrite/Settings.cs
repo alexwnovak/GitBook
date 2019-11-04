2a74c3fc9453ed83f58096581af99b5215022bdb
Alex Novak <alexwnovak@gmail.com>
2019-11-03 22:27:19 -0500

    +Radial color

--- src/GitWrite/Settings.cs
+++ src/GitWrite/Settings.cs
@@ -7,5 +7,6 @@ namespace GitWrite
       public Color WindowTintColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#EEE" );
       public double WindowTintOpacity { get; set; } = 0.7;
       public Color SeparatorColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#CCC" );
+      public Color RadialColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#FFCF9E" );
    }
 }
