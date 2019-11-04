1d57f2f6aa2d640bbe8b4cb5080c612ecf642ac9
Alex Novak <alexwnovak@gmail.com>
2019-11-03 22:07:13 -0500

    +Settings class with a few values--we'll add more

---
+++ src/GitWrite/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+
+namespace GitWrite
+{
+   public class Settings
+   {
+      public Color WindowTintColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#EEE" );
+      public double WindowTintOpacity { get; set; } = 0.7;
+   }
+}
