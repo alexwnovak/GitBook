29e150bafe1b417b9f2d3781be97762966be8a96
Alex Novak <alexwnovak@gmail.com>
2019-11-03 22:31:25 -0500

    Hooking up subject foreground color

--- src/GitWrite/Settings.cs
+++ src/GitWrite/Settings.cs
@@ -8,5 +8,6 @@ namespace GitWrite
       public double WindowTintOpacity { get; set; } = 0.7;
       public Color SeparatorColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#CCC" );
       public Color RadialColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#FFCF9E" );
+      public Color SubjectTextColor { get; set; } = (Color) ColorConverter.ConvertFromString( "#000" );
    }
 }
